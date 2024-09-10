0cd5120c2f16d7405f31d87d10cd1edb09375173
chuxixue <chuxixue@huawei.com>
2024-09-10 11:06:27 +0800

    协同头动失效
    Signed-off-by: chuxixue <chuxixue@huawei.com>

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -7078,6 +7078,7 @@ void AudioPolicyService::UpdateA2dpOffloadFlag(const std::vector<Bluetooth::A2dp
         std::vector<int32_t> allSessions;
         GetAllRunningStreamSession(allSessions);
         OffloadStartPlaying(allSessions);
+        UpdateEffectBtOffloadSupported(true);
     }
 }
 #endif
