ee11de9c59270d14f1f1268a4bb54597ad8ca702
Wang Hengshen <wanghengshen1@huawei.com>
2024-09-10 20:11:58 +0800

    Revert "asynchronously set audio scene in interrupt service"
    This reverts commit 1d89b1f55aa3fd798eb85006f7709584e92fcd5a.
    
    Signed-off-by: Wang Hengshen <wanghengshen1@huawei.com>

--- services/audio_policy/server/src/service/interrupt/audio_interrupt_service.cpp
+++ services/audio_policy/server/src/service/interrupt/audio_interrupt_service.cpp
@@ -1348,10 +1348,7 @@ void AudioInterruptService::UpdateAudioSceneFromInterrupt(const AudioScene audio
             AUDIO_ERR_LOG("unexpected changeType: %{public}d", changeType);
             return;
     }
-    std::thread setAudioSceneThread([this, audioScene] {
-        this->policyServer_->SetAudioSceneInternal(audioScene);
-    });
-    setAudioSceneThread.detach();
+    policyServer_->SetAudioSceneInternal(audioScene);
 }
 
 std::list<std::pair<AudioInterrupt, AudioFocuState>> AudioInterruptService::SimulateFocusEntry(const int32_t zoneId)
