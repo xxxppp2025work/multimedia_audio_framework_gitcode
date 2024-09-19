3acf2322aa977d66b3013b0d5ba5f109517efd52
caochuan <caochuan@huawei.com>
2024-09-19 18:22:55 +0800

    use safethread function.
    Signed-off-by: caochuan <caochuan@huawei.com>

--- frameworks/js/napi/audiomanager/callback/napi_audio_ringermode_callback.cpp
+++ frameworks/js/napi/audiomanager/callback/napi_audio_ringermode_callback.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <thread>
 #ifndef LOG_TAG
 #define LOG_TAG "NapiAudioRingerModeCallback"
 #endif
@@ -22,13 +23,8 @@
 #include "napi_param_utils.h"
 #include "napi_audio_error.h"
 #include "napi_audio_enum.h"
-#ifdef SUPPORT_CONTAINER_SCOPE
-#include "core/common/container_scope.h"
-#endif
+#include "napi/native_api.h"
 
-#ifdef SUPPORT_CONTAINER_SCOPE
-using OHOS::Ace::ContainerScope;
-#endif
 namespace OHOS {
 namespace AudioStandard {
 NapiAudioRingerModeCallback::NapiAudioRingerModeCallback(napi_env env)
@@ -123,58 +119,57 @@ static NapiAudioEnum::AudioRingMode GetJsAudioRingMode(int32_t ringerMode)
     return result;
 }
 
+void NapiAudioRingerModeCallback::SafeJsCallbackInterruptWork(napi_env env, napi_value js_cb, void* context, void* data)
+{
+    CHECK_AND_RETURN_LOG(data != nullptr, "data is nullptr");
+    AudioRingerModeJsCallback *event = reinterpret_cast<AudioRingerModeJsCallback *>(data);
+    CHECK_AND_RETURN_LOG(event != nullptr, "event is nullptr");
+    std::string request = event->callbackName;
+    CHECK_AND_RETURN_LOG(event->callback != nullptr, "event is nullptr");
+    napi_ref callback = event->callback->cb_;
+    napi_handle_scope scope = nullptr;
+    napi_open_handle_scope(env, &scope);
+    CHECK_AND_RETURN_LOG(scope != nullptr, "scope is nullptr");
+    AUDIO_INFO_LOG("SafeJsCallbackInterruptWork: safe ringer mode callback working.");
+
+    do {
+        napi_value jsCallback = nullptr;
+        napi_status nstatus = napi_get_reference_value(env, callback, &jsCallback);
+        CHECK_AND_BREAK_LOG(nstatus == napi_ok && jsCallback != nullptr, "%{public}s get reference value fail",
+            request.c_str());
+        napi_value args[ARGS_ONE] = { nullptr };
+        NapiParamUtils::SetValueInt32(env, GetJsAudioRingMode(event->ringerMode), args[PARAM0]);
+        CHECK_AND_BREAK_LOG(nstatus == napi_ok && args[PARAM0] != nullptr,
+            "%{public}s fail to create ringer mode callback", request.c_str());
+
+        const size_t argCount = ARGS_ONE;
+        napi_value result = nullptr;
+        nstatus = napi_call_function(env, nullptr, jsCallback, argCount, args, &result);
+        CHECK_AND_BREAK_LOG(nstatus == napi_ok, "%{public}s fail to call ringer mode callback",
+            request.c_str());
+    } while (0);
+    napi_close_handle_scope(env, scope);
+}
+
 void NapiAudioRingerModeCallback::OnJsCallbackRingerMode(std::unique_ptr<AudioRingerModeJsCallback> &jsCb)
 {
     if (jsCb.get() == nullptr) {
-        AUDIO_ERR_LOG("OnJsCallbackRendererState: jsCb.get() is null");
+        AUDIO_ERR_LOG("NapiAudioRingerModeCallback: OnJsCallbackRingerMode: jsCb.get() is null");
         return;
     }
 
     AudioRingerModeJsCallback *event = jsCb.get();
-    auto task = [event
-#ifdef SUPPORT_CONTAINER_SCOPE
-, scopeId = ContainerScope::CurrentId()
-#endif
-        ]() {
-#ifdef SUPPORT_CONTAINER_SCOPE
-            ContainerScope cs(scopeId);
-#endif
-        std::shared_ptr<AudioRingerModeJsCallback> context(
-            static_cast<AudioRingerModeJsCallback*>(event),
-            [](AudioRingerModeJsCallback* ptr) {
-                delete ptr;
-        });
-        CHECK_AND_RETURN_LOG(event != nullptr, "event is nullptr");
-        std::string request = event->callbackName;
-        CHECK_AND_RETURN_LOG(event->callback != nullptr, "event is nullptr");
-        napi_env env = event->callback->env_;
-        napi_ref callback = event->callback->cb_;
-        napi_handle_scope scope = nullptr;
-        napi_open_handle_scope(env, &scope);
-        CHECK_AND_RETURN_LOG(scope != nullptr, "scope is nullptr");
-        do {
-            napi_value jsCallback = nullptr;
-            napi_status nstatus = napi_get_reference_value(env, callback, &jsCallback);
-            CHECK_AND_BREAK_LOG(nstatus == napi_ok && jsCallback != nullptr, "%{public}s get reference value fail",
-                request.c_str());
-            napi_value args[ARGS_ONE] = { nullptr };
-            NapiParamUtils::SetValueInt32(env, GetJsAudioRingMode(event->ringerMode), args[PARAM0]);
-            CHECK_AND_BREAK_LOG(nstatus == napi_ok && args[PARAM0] != nullptr,
-                "%{public}s fail to create ringer mode callback", request.c_str());
+    napi_value cbName;
+    napi_create_string_utf8(event->callback->env_, event->callbackName.c_str(), event->callbackName.length(), &cbName);
+    napi_create_threadsafe_function(event->callback->env_, nullptr, nullptr, cbName, 0, 1, nullptr, nullptr, nullptr, SafeJsCallbackInterruptWork, &tsfn);
 
-            const size_t argCount = ARGS_ONE;
-            napi_value result = nullptr;
-            nstatus = napi_call_function(env, nullptr, jsCallback, argCount, args, &result);
-            CHECK_AND_BREAK_LOG(nstatus == napi_ok, "%{public}s fail to call ringer mode callback",
-                request.c_str());
-        } while (0);
-        napi_close_handle_scope(env, scope);
-    };
-    if (napi_status::napi_ok != napi_send_event(env_, task, napi_eprio_immediate)) {
-        AUDIO_ERR_LOG("OnJsCallbackRingerMode: Failed to SendEvent");
-    } else {
-        jsCb.release();
-    }
+    std::thread safeCallThread([event]() {
+        AUDIO_INFO_LOG("OnJsCallbackRingerMode: start safe working thread.");
+        napi_acquire_threadsafe_function(tsfn);
+        napi_call_threadsafe_function(tsfn, event, napi_tsfn_blocking);
+    });
+
+    safeCallThread.detach();
 }
 } // namespace AudioStandard
 } // namespace OHOS
