6abe7d1d963f8c09bcc98200742a078814ed2d4e
Jvgang <yujugang1@h-partners.com>
2024-09-22 08:59:11 +0000

    新增音频内录权限：ohos.permission.CAPTURE_PLAYBACK!同步蓝区5.0！
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- services/audio_service/server/src/audio_server.cpp
+++ services/audio_service/server/src/audio_server.cpp
@@ -1340,14 +1340,14 @@ void AudioServer::ResetRecordConfig(AudioProcessConfig &config)
         if (PermissionUtil::VerifyPermission(CAPTURE_PLAYBACK_PERMISSION, config.appInfo.appTokenId)) {
             AUDIO_INFO_LOG("CAPTURE_PLAYBACK permission granted");
             config.innerCapMode = MODERN_INNER_CAP;
-        } else if (config.callerUid == MEDIA_SERVICE_UID) {
-            config.innerCapMode = MODERN_INNER_CAP;
-        } else if (config.callerUid == VASSISTANT_UID) {
+        } else if (config.callerUid == MEDIA_SERVICE_UID || config.callerUid == VASSISTANT_UID) {
             config.innerCapMode = MODERN_INNER_CAP;
         } else if (GetHapBuildApiVersion(config.callerUid) >= MODERN_INNER_API_VERSION) { // check build api-version
             config.innerCapMode = LEGACY_MUTE_CAP;
         }
+        AUDIO_INFO_LOG("callerUid %{public}d, innerCapMode %{public}d", config.callerUid, config.innerCapMode);
     } else {
+        AUDIO_INFO_LOG("CAPTURE_PLAYBACK permission denied");
         config.isInnerCapturer = false;
     }
 #ifdef AUDIO_BUILD_VARIANT_ROOT
