3d649553902e4bacad6c7e3a0da6e5d8d9bcac03
hejunjieru <hejunjie16@h-partners.com>
2024-09-26 16:27:01 +0800

    fix audio_server-sys_freeze
    Signed-off-by: hejunjieru <hejunjie16@h-partners.com>

--- frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
+++ frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
@@ -705,6 +705,7 @@ int32_t AudioEffectChainManager::UpdateMultichannelConfig(const std::string &sce
 
 int32_t AudioEffectChainManager::InitAudioEffectChainDynamic(const std::string &sceneType)
 {
+    AUDIO_INFO_LOG("begin InitAudioEffectChainDynamic");
     std::lock_guard<std::recursive_mutex> lock(dynamicMutex_);
     CHECK_AND_RETURN_RET_LOG(isInitialized_, ERROR, "has not been initialized");
     CHECK_AND_RETURN_RET_LOG(sceneType != "", ERROR, "null sceneType");
