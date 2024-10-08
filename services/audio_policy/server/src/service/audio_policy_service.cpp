6b023b073d435cd6132d7436c189cb0e98edc3d4
WangWanliang <wangwanliang1@h-partners.com>
2024-10-08 11:21:57 +0800

    添加获取当前UV值的设备类型打印日志
    Signed-off-by: WangWanliang <wangwanliang1@h-partners.com>

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -8110,7 +8110,7 @@ float AudioPolicyService::GetMaxAmplitude(const int32_t deviceId)
         IPCSkeleton::SetCallingIdentity(identity);
         return inputMaxAmplitude;
     }
-
+    AUDIO_INFO_LOG("Incoming device type: %{public}d", deviceId);
     return 0;
 }
 
