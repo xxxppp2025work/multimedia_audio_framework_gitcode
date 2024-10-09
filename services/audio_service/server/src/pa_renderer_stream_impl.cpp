2ccb442f6ee11abbc7ce588db69963c583be5d62
hejunjieru <hejunjie16@h-partners.com>
2024-10-09 10:17:16 +0800

    fix membuffer
    Signed-off-by: hejunjieru <hejunjie16@h-partners.com>

--- services/audio_service/server/src/pa_renderer_stream_impl.cpp
+++ services/audio_service/server/src/pa_renderer_stream_impl.cpp
@@ -203,6 +203,13 @@ int32_t PaRendererStreamImpl::Pause()
     operation = pa_stream_cork(paStream_, 1, PAStreamPauseSuccessCb, reinterpret_cast<void *>(this));
     pa_operation_unref(operation);
     pa_threaded_mainloop_unlock(mainloop_);
+
+    if (effectMode_ = EFFECT_DEFAULT) {
+        AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
+        if (audioEffectChainManager != nullptr) {
+            audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
+        }
+    }
     return SUCCESS;
 }
 
@@ -228,8 +235,11 @@ int32_t PaRendererStreamImpl::Flush()
         return ERR_OPERATION_FAILED;
     }
     Trace trace("PaRendererStreamImpl::InitAudioEffectChainDynamic");
-    if (effectMode_ == EFFECT_DEFAULT) {
-        AudioEffectChainManager::GetInstance()->InitAudioEffectChainDynamic(effectSceneName_);
+    if (effectMode_ = EFFECT_DEFAULT) {
+        AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
+        if (audioEffectChainManager != nullptr) {
+            audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
+        }
     }
     pa_operation_unref(operation);
     return SUCCESS;
@@ -270,6 +280,12 @@ int32_t PaRendererStreamImpl::Stop()
         reinterpret_cast<void *>(this));
     CHECK_AND_RETURN_RET_LOG(operation != nullptr, ERR_OPERATION_FAILED, "pa_stream_cork operation is null");
     pa_operation_unref(operation);
+    if (effectMode_ = EFFECT_DEFAULT) {
+        AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
+        if (audioEffectChainManager != nullptr) {
+            audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
+        }
+    }
     return SUCCESS;
 }
 
@@ -280,6 +296,13 @@ int32_t PaRendererStreamImpl::Release()
         statusCallback->OnStatusUpdate(OPERATION_RELEASED);
     }
     state_ = RELEASED;
+
+    if (effectMode_ = EFFECT_DEFAULT) {
+        AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
+        if (audioEffectChainManager != nullptr) {
+            audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
+        }
+    }
     return SUCCESS;
 }
 
