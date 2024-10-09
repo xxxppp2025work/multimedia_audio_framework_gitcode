a46fa14c31889f95beeeb2e1a8323bfe59ae6c80
hejunjieru <hejunjie16@h-partners.com>
2024-10-09 10:34:24 +0800

    fix membuffer 5.0.1
    Signed-off-by: hejunjieru <hejunjie16@h-partners.com>

--- services/audio_service/server/src/pa_renderer_stream_impl.cpp
+++ services/audio_service/server/src/pa_renderer_stream_impl.cpp
@@ -203,7 +203,7 @@ int32_t PaRendererStreamImpl::Pause()
     operation = pa_stream_cork(paStream_, 1, PAStreamPauseSuccessCb, reinterpret_cast<void *>(this));
     pa_operation_unref(operation);
     pa_threaded_mainloop_unlock(mainloop_);
-    if (effectMode_ = EFFECT_DEFAULT) {
+    if (effectMode_ == EFFECT_DEFAULT) {
         AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
         if (audioEffectChainManager != nullptr) {
             audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
@@ -238,7 +238,7 @@ int32_t PaRendererStreamImpl::Flush()
         AudioEffectChainManager::GetInstance()->InitAudioEffectChainDynamic(effectSceneName_);
     }
     pa_operation_unref(operation);
-    if (effectMode_ = EFFECT_DEFAULT) {
+    if (effectMode_ == EFFECT_DEFAULT) {
         AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
         if (audioEffectChainManager != nullptr) {
             audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
@@ -282,7 +282,7 @@ int32_t PaRendererStreamImpl::Stop()
         reinterpret_cast<void *>(this));
     CHECK_AND_RETURN_RET_LOG(operation != nullptr, ERR_OPERATION_FAILED, "pa_stream_cork operation is null");
     pa_operation_unref(operation);
-    if (effectMode_ = EFFECT_DEFAULT) {
+    if (effectMode_ == EFFECT_DEFAULT) {
         AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
         if (audioEffectChainManager != nullptr) {
             audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
@@ -298,7 +298,7 @@ int32_t PaRendererStreamImpl::Release()
         statusCallback->OnStatusUpdate(OPERATION_RELEASED);
     }
     state_ = RELEASED;
-    if (effectMode_ = EFFECT_DEFAULT) {
+    if (effectMode_ == EFFECT_DEFAULT) {
         AudioEffectChainManager *audioEffectChainManager = AudioEffectChainManager::GetInstance();
         if (audioEffectChainManager != nullptr) {
             audioEffectChainManager->InitAudioEffectChainDynamic(effectSceneName_);
