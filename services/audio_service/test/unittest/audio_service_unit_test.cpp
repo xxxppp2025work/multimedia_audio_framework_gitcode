82d07884f1909483a0f632f353194a31c227f5a2
baixin12345 <baixin14@huawei.com>
2024-10-14 18:53:04 +0000

    fix ut
    Signed-off-by: baixin12345 <baixin14@huawei.com>

--- services/audio_service/test/unittest/audio_service_unit_test.cpp
+++ services/audio_service/test/unittest/audio_service_unit_test.cpp
@@ -139,7 +139,7 @@ HWTEST(AudioServiceUnitTest, AudioManagerProxy_001, TestSize.Level1)
 
     bool state = false;
     ret = audioManagerProxy->SetCaptureSilentState(state);
-    EXPECT_EQ(ERROR_62980101, ret);
+    EXPECT_EQ(SUCCESS, ret);
 
     bool result = audioManagerProxy->CreatePlaybackCapturerManager();
     EXPECT_EQ(result, SUCCESS);
