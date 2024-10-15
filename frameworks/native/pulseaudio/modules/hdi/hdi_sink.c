a70eb23efef84d7e7f5168e7d8252f8df51ed869
wangjiajun <wangjiajun31@huawei.com>
2024-10-15 11:30:51 +0800

    解决分布式设备croked状态下，流切换状态sink不感知导致不发送underflow回调问题
    Signed-off-by: wangjiajun <wangjiajun31@huawei.com>

--- frameworks/native/pulseaudio/modules/hdi/hdi_sink.c
+++ frameworks/native/pulseaudio/modules/hdi/hdi_sink.c
@@ -2718,6 +2718,13 @@ static void PaInputStateChangeCb(pa_sink_input *i, pa_sink_input_state_t state)
     pa_assert(i);
     pa_sink_input_assert_ref(i);
     pa_assert(i->sink);
+
+    const bool corking = i->thread_info.state == PA_SINK_INPUT_RUNNING && state == PA_SINK_INPUT_CORKED;
+    const bool starting = i->thread_info.state == PA_SINK_INPUT_CORKED && state == PA_SINK_INPUT_RUNNING;
+    const bool stopping = state == PA_SINK_INPUT_UNLINKED;
+
+    corking ? pa_atomic_store(&i->isFirstReaded, 0) : (void)0;
+
     if (!strcmp(i->sink->name, SINK_NAME_INNER_CAPTURER) ||
         !strcmp(i->sink->name, SINK_NAME_REMOTE_CAST_INNER_CAPTURER)) {
         ResetFadeoutPause(i, state);
@@ -2748,12 +2755,6 @@ static void PaInputStateChangeCb(pa_sink_input *i, pa_sink_input_state_t state)
         pa_sink_input_send_event(i, "state_changed", propList);
         pa_proplist_free(propList);
     }
-
-    const bool corking = i->thread_info.state == PA_SINK_INPUT_RUNNING && state == PA_SINK_INPUT_CORKED;
-    const bool starting = i->thread_info.state == PA_SINK_INPUT_CORKED && state == PA_SINK_INPUT_RUNNING;
-    const bool stopping = state == PA_SINK_INPUT_UNLINKED;
-
-    corking ? pa_atomic_store(&i->isFirstReaded, 0) : (void)0;
 
     if (!corking && !starting && !stopping) {
         AUDIO_WARNING_LOG("PaInputStateChangeCb, input state change: invalid");
