e267ae9a7af35638bfe72ec8c0dcd8791436732a
baixin12345 <baixin14@huawei.com>
2024-10-15 00:44:40 +0000

    update services/audio_service/test/unittest/audio_service_unit_test.cpp. fix ut
    Signed-off-by: baixin12345 <baixin14@huawei.com>

--- services/audio_service/test/unittest/audio_service_unit_test.cpp
+++ services/audio_service/test/unittest/audio_service_unit_test.cpp
@@ -34,7 +34,6 @@ std::unique_ptr<AudioManagerProxy> audioManagerProxy;
 std::shared_ptr<AudioProcessInClient> processClient_;
 const int32_t TEST_RET_NUM = 0;
 const int32_t RENDERER_FLAGS = 0;
-constexpr int32_t ERROR_62980101 = -62980101;
 
 class AudioServiceUnitTest : public testing::Test {
 public:
