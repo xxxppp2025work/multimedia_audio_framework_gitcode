b3aa9e7d013fb31f86a99d1b75bccc7ba4f9f440
shanliang <shanliang6@huawei.com>
2024-10-15 19:19:35 +0800

    fix error
    Signed-off-by: shanliang <shanliang6@huawei.com>

--- services/audio_service/server/src/pa_renderer_stream_impl.cpp
+++ services/audio_service/server/src/pa_renderer_stream_impl.cpp
@@ -268,26 +268,6 @@ int32_t PaRendererStreamImpl::Stop()
         return ERR_ILLEGAL_STATE;
     }
 
-    pa_proplist *propList = pa_proplist_new();
-    if (propList != nullptr) {
-        pa_proplist_sets(propList, "fadeoutPause", "1");
-        pa_operation *updatePropOperation = pa_stream_proplist_update(paStream_, PA_UPDATE_REPLACE, propList,
-            nullptr, nullptr);
-        pa_proplist_free(propList);
-        CHECK_AND_RETURN_RET_LOG(updatePropOperation != nullptr, ERR_OPERATION_FAILED, "updatePropOp is nullptr");
-        pa_operation_unref(updatePropOperation);
-        AUDIO_INFO_LOG("pa_stream_proplist_update done");
-        if (!offloadEnable_) {
-            palock.Unlock();
-            {
-                std::unique_lock<std::mutex> lock(fadingMutex_);
-                const int32_t WAIT_TIME_MS = 20;
-                fadingCondition_.wait_for(lock, std::chrono::milliseconds(WAIT_TIME_MS));
-            }
-            palock.Relock();
-        }
-    }
-
     pa_operation *operation = pa_stream_cork(paStream_, 1, PaRendererStreamImpl::PAStreamAsyncStopSuccessCb,
         reinterpret_cast<void *>(this));
     CHECK_AND_RETURN_RET_LOG(operation != nullptr, ERR_OPERATION_FAILED, "pa_stream_cork operation is null");
