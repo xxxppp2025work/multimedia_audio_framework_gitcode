52ac3755ba29f9d45728cc9ddfaeb8a3fe2bfb16
秦川钞 <qinchuanchao@h-partners.com>
2024-10-15 15:42:17 +0800

    静音方案管控后台起流
    Signed-off-by: chumouzhi <chumouzhi@h-partners.com>

--- frameworks/native/audiorenderer/include/audio_renderer_private.h
+++ frameworks/native/audiorenderer/include/audio_renderer_private.h
@@ -181,6 +181,7 @@ private:
     void WriteUnderrunEvent() const;
     IAudioStream::StreamClass GetPreferredStreamClass(AudioStreamParams audioStreamParams);
     bool IsDirectVoipParams(const AudioStreamParams &audioStreamParams);
+    bool IsAllowedStartBackgroud();
 
     std::shared_ptr<AudioInterruptCallback> audioInterruptCallback_ = nullptr;
     std::shared_ptr<AudioStreamCallback> audioStreamCallback_ = nullptr;
--- frameworks/native/audiorenderer/src/audio_renderer.cpp
+++ frameworks/native/audiorenderer/src/audio_renderer.cpp
@@ -410,6 +410,26 @@ IAudioStream::StreamClass AudioRendererPrivate::GetPreferredStreamClass(AudioStr
     return IAudioStream::PA_STREAM;
 }
 
+bool AudioRendererPrivate::IsAllowedStartBackgroud()
+{
+    bool ret = false;
+#ifdef AVSESSION_ENABLE
+    ret = OHOS::AVSession::AVSessionManager::GetInstance().IsAudioPlaybackAllowed(appInfo_.appUid, appInfo_.appPid);
+    if (ret) {
+        AUDIO_INFO_LOG("AVSession IsAudioPlaybackAllowed is: %{public}d", ret);
+        return ret;
+    }
+    if (std::count(EXEMPT_MUTE_STREAM_USAGE.begin(), EXEMPT_MUTE_STREAM_USAGE.end(), rendererInfo_.streamUsage) != 0) {
+        ret =true;
+        AUDIO_INFO_LOG("%{public}d is EXEMPT_MUTE_STREAM_USAGE", rendererInfo_.streamUsage);
+        return ret;
+    }
+#else
+    ret = true;
+#endif
+    return ret;
+}
+
 bool AudioRendererPrivate::IsDirectVoipParams(const AudioStreamParams &audioStreamParams)
 {
     // VoIP derect only supports 16K and 48K sampling rate.
@@ -611,7 +631,8 @@ bool AudioRendererPrivate::Start(StateChangeCmdType cmdType)
     std::lock_guard<std::shared_mutex> lock(rendererMutex_);
     AUDIO_INFO_LOG("StreamClientState for Renderer::Start. id: %{public}u, streamType: %{public}d, "\
         "interruptMode: %{public}d", sessionID_, audioInterrupt_.audioFocusType.streamType, audioInterrupt_.mode);
-
+    CHECK_AND_RETURN_RET_LOG(IsAllowedStartBackgroud(), false,
+        "Start failed. IsAllowedStartBackgroud is:%{public}d", IsAllowedStartBackgroud());
     RendererState state = GetStatus();
     CHECK_AND_RETURN_RET_LOG((state == RENDERER_PREPARED) || (state == RENDERER_STOPPED) || (state == RENDERER_PAUSED),
         false, "Start failed. Illegal state:%{public}u", state);
