0f5ca83b22a2731f6dc57d775bb22f852f54d543
蒋志庆 <jiangzhiqing4@h-partners.com>
2024-10-16 17:38:34 +0800

    音效持久化
    Signed-off-by: 蒋志庆 <jiangzhiqing4@h-partners.com>

--- services/audio_policy/client/include/audio_policy_proxy.h
+++ services/audio_policy/client/include/audio_policy_proxy.h
@@ -337,11 +337,9 @@ public:
 
     int32_t GetAudioEffectProperty(AudioEffectPropertyArray &propertyArray) override;
 
-    int32_t SetAudioEnhanceProperty(const AudioEnhancePropertyArray &propertyArray
-        DeviceType deviceType = DEVICE_TYPE_NONE) override;
+    int32_t SetAudioEnhanceProperty(const AudioEnhancePropertyArray &propertyArray) override;
 
-    int32_t GetAudioEnhanceProperty(AudioEnhancePropertyArray &propertyArray,
-        DeviceType deviceType = DEVICE_TYPE_NONE) override;
+    int32_t GetAudioEnhanceProperty(AudioEnhancePropertyArray &propertyArray) override;
 
 private:
     static inline BrokerDelegator<AudioPolicyProxy> mDdelegator;
