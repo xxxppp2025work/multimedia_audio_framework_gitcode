cd2a0e12488ffd4ce30d25a474a88498f3eae533
lizihao <lizihao56@h-partners.com>
2024-10-22 21:10:36 +0800

    debug
    Signed-off-by: lizihao <lizihao56@h-partners.com>

--- frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
+++ frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
@@ -494,7 +494,7 @@ bool AudioEffectChainManager::ExistAudioEffectChain(const std::string &sceneType
 }
 
 int32_t AudioEffectChainManager::ApplyAudioEffectChain(const std::string &sceneType,
-    const std::unique_ptr<EffectBufferAttr> &bufferAttr)
+    std::unique_ptr<EffectBufferAttr> &bufferAttr)
 {
     std::string sceneTypeAndDeviceKey = sceneType + "_&_" + GetDeviceTypeName();
     size_t totLen = static_cast<size_t>(bufferAttr->frameLen * bufferAttr->numChans * sizeof(float));
