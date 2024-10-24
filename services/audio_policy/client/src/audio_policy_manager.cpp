ced743c021ca877467cfba380709ffb9c94d2b17
heyabinghyb <heyabing@h-partners.com>
2024-10-24 13:10:03 +0000

    检视意见修改
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- services/audio_policy/client/src/audio_policy_manager.cpp
+++ services/audio_policy/client/src/audio_policy_manager.cpp
@@ -1246,8 +1246,9 @@ int32_t AudioPolicyManager::RegisterAudioPolicyServerDiedCb(const int32_t client
     const std::weak_ptr<AudioRendererPolicyServiceDiedCallback> &callback)
 {
     std::lock_guard<std::mutex> lockCbMap(g_cBMapMutex);
-    //Check if clientPid already exists, prevent secondary anomalies
+    // Check if clientPid already exists, prevent secondary anomalies
     if (rendererCBMap_.find(clientPid) != rendererCBMap_.end()) {
+        AUDIO_ERR_LOG("RegisterAudioPolicyServerDiedCb: clientPid already exists");
         return ERROR;
     }
     rendererCBMap_[clientPid] = callback;
