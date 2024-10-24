ec2104da2beb949e908c61bde54c1de533e6c714
heyabinghyb <heyabing@h-partners.com>
2024-10-24 04:05:24 +0000

    检查是否二次执行RegisterAudioPolicyServerDiedCb
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- services/audio_policy/client/src/audio_policy_manager.cpp
+++ services/audio_policy/client/src/audio_policy_manager.cpp
@@ -1246,6 +1246,10 @@ int32_t AudioPolicyManager::RegisterAudioPolicyServerDiedCb(const int32_t client
     const std::weak_ptr<AudioRendererPolicyServiceDiedCallback> &callback)
 {
     std::lock_guard<std::mutex> lockCbMap(g_cBMapMutex);
+    //Check if clientPid already exists, prevent secondary anomalies
+    if (rendererCBMap_.find(clientPid) != rendererCBMap_.end()) {
+        return ERROR;
+    }
     rendererCBMap_[clientPid] = callback;
     return SUCCESS;
 }
