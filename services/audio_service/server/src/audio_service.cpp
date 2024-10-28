2f87972d38fea82111ccbf575ee3afb22b0373d7
yangshuxiang <yangshuxiang@huawei.com>
2024-10-28 06:27:11 +0000

    fix confilct
    Signed-off-by: yangshuxiang <yangshuxiang@huawei.com>

--- services/audio_service/server/src/audio_service.cpp
+++ services/audio_service/server/src/audio_service.cpp
@@ -34,6 +34,7 @@ namespace AudioStandard {
 static uint64_t g_id = 1;
 static const uint32_t NORMAL_ENDPOINT_RELEASE_DELAY_TIME_MS = 3000; // 3s
 static const uint32_t A2DP_ENDPOINT_RELEASE_DELAY_TIME = 3000; // 3s
+static const uint32_t A2DP_ENDPOINT_RE_CREATE_RELEASE_DELAY_TIME = 200; // 200ms
 static const int32_t MEDIA_SERVICE_UID = 1013;
 
 AudioService *AudioService::GetInstance()
