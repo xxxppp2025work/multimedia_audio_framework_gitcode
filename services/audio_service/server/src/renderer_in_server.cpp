75ebdfa588840a8935caa970afe818ace1df5fd5
任军旗 <renjunqi3@huawei.com>
2024-10-30 08:19:35 +0000

    修改应用后台被冻结，唤醒后恢复播放，恢复一小段之后又被冻结导致杂音问题
    Signed-off-by: 任军旗 <renjunqi3@huawei.com>

--- services/audio_service/server/src/renderer_in_server.cpp
+++ services/audio_service/server/src/renderer_in_server.cpp
@@ -371,12 +371,16 @@ void RendererInServer::VolumeHandle(BufferDesc &desc)
     } else {
         applyVolume = audioServerBuffer_->GetStreamVolume();
     }
-    float duckVolume_ = audioServerBuffer_->GetDuckFactor();
+    float duckVolume = audioServerBuffer_->GetDuckFactor();
+    float muteVolume = audioServerBuffer_->GetMuteFactor();
     if (!IsVolumeSame(MAX_FLOAT_VOLUME, lowPowerVolume_, AUDIO_VOLOMUE_EPSILON)) {
         applyVolume *= lowPowerVolume_;
     }
-    if (!IsVolumeSame(MAX_FLOAT_VOLUME, duckVolume_, AUDIO_VOLOMUE_EPSILON)) {
-        applyVolume *= duckVolume_;
+    if (!IsVolumeSame(MAX_FLOAT_VOLUME, duckVolume, AUDIO_VOLOMUE_EPSILON)) {
+        applyVolume *= duckVolume;
+    }
+    if (!IsVolumeSame(MAX_FLOAT_VOLUME, muteVolume, AUDIO_VOLOMUE_EPSILON)) {
+        applyVolume *= muteVolume;
     }
 
     if (silentModeAndMixWithOthers_) {
