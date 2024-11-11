aaa6cbec6a5ecc65bd335693a14a6ec066730620
wangtao <wangtao570@huawei.com>
2024-11-11 02:31:43 +0000

    修复来铃声时短暂音乐双响
    Signed-off-by: wangtao <wangtao570@huawei.com>
    Change-Id: I58afd26c9f26c84d25fc7697c9c4bef72233ae28

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -59,7 +59,8 @@ static const std::string PIPE_WAKEUP_INPUT = "wakeup_input";
 static const int64_t CALL_IPC_COST_TIME_MS = 20000000; // 20ms
 static const int32_t WAIT_OFFLOAD_CLOSE_TIME_S = 10; // 10s
 static const int64_t OLD_DEVICE_UNAVALIABLE_MUTE_MS = 1000000; // 1s
-static const int64_t OLD_DEVICE_UNAVALIABLE_MUTE_SLEEP_MS = 150000; // 150ms
+static const int64_t OLD_DEVICE_UNAVALIABLE_MUTE_SLEEP_MS = 300000; // 300ms
+static const int64_t DOUBLE_RINGING_MUTE_SLEEP_MS = 300000; // 300ms
 static const int64_t SELECT_DEVICE_MUTE_MS = 200000; // 200ms
 static const int64_t SELECT_OFFLOAD_DEVICE_MUTE_MS = 400000; // 400ms
 static const int64_t NEW_DEVICE_AVALIABLE_MUTE_MS = 400000; // 400ms
@@ -2148,7 +2149,17 @@ void AudioPolicyService::MuteSinkPortForSwtichDevice(unique_ptr<AudioRendererCha
     vector<std::unique_ptr<AudioDeviceDescriptor>>& outputDevices, const AudioStreamDeviceChangeReasonExt reason)
 {
     Trace trace("AudioPolicyService::MuteSinkPortForSwtichDevice");
-    if (outputDevices.size() != 1) return;
+
+    if (outputDevices.size() != 1) {
+        if (IsStreamActive(STREAM_MUSIC)) {
+            Trace trace("AudioPolicyService::DoubleRingingForSwtichDevice");
+            MuteSinkPort("null", PRIMARY_SPEAKER, AudioStreamDeviceChangeReasonExt::ExtEnum::OVERRODE);
+            AUDIO_INFO_LOG("double ringing mute primary wait %{public}d us", DOUBLE_RINGING_MUTE_SLEEP_MS);
+            usleep(DOUBLE_RINGING_MUTE_SLEEP_MS);
+        }
+        return;
+    }
+
     if (outputDevices.front()->isSameDevice(rendererChangeInfo->outputDeviceInfo)) return;
 
     moveDeviceFinished_ = false;
