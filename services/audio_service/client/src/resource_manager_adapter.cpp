42844f9b4c482f756570c24dea8966cf8fab1c38
wangm <wangmeng244@huawei.com>
2024-11-18 11:22:40 +0800

    release system resource manager
    Signed-off-by: wangm <wangmeng244@huawei.com>

---
+++ services/audio_service/client/src/resource_manager_adapter.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef LOG_TAG
+#define LOG_TAG "ResourceManagerAdapter"
+#endif
+
+#include "resource_manager_adapter.h"
+
+#include "locale_config.h"
+#include "audio_log.h"
+#include "audio_error.h"
+
+namespace OHOS {
+namespace AudioStandard {
+
+ResourceManagerAdapter::ResourceManagerAdapter()
+{
+    AUDIO_INFO_LOG("ResourceManagerAdapter construct");
+}
+
+ResourceManagerAdapter::~ResourceManagerAdapter()
+{
+    AUDIO_INFO_LOG("ResourceManagerAdapter destroy");
+}
+
+ResourceManagerAdapter *ResourceManagerAdapter::GetInstance()
+{
+    static ResourceManagerAdapter resourceManagerAdapter;
+    return &resourceManagerAdapter;
+}
+
+void ResourceManagerAdapter::InitResourceManager()
+{
+    if (!resourceManager_) {
+        AUDIO_INFO_LOG("InitResourceManager");
+        resourceManager_ = Global::Resource::GetSystemResourceManagerNoSandBox();
+    }
+    if (!resConfig_) {
+        resConfig_ = Global::Resource::CreateResConfig();
+    }
+    RefreshResConfig();
+}
+
+void ResourceManagerAdapter::RefreshResConfig()
+{
+    std::string language = Global::I18n::LocaleConfig::GetSystemLanguage();
+    UErrorCode status = U_ZERO_ERROR;
+    icu::Locale locale = icu::Locale::forLanguageTag(language, status);
+    if (status != U_ZERO_ERROR) {
+        AUDIO_INFO_LOG("forLanguageTag failed, errCode:%{public}d", status);
+    }
+    resConfig_->SetLocaleInfo(locale.getLanguage(), locale.getScript(), locale.getCountry());
+    resourceManager_->UpdateResConfig(*resConfig_);
+    AUDIO_INFO_LOG("RefreshResConfig success");
+}
+
+void ResourceManagerAdapter::ReleaseSystemResourceManager()
+{
+    std::lock_guard<std::mutex> lock(resourceManagerMutex_);
+    if (resourceManager_) {
+        Global::Resource::ReleaseSystemResourceManager();
+        resourceManager_ = nullptr;
+    }
+    
+    if (!resConfig_) {
+        delete resConfig_;
+        resConfig_ = nullptr;
+    }
+    AUDIO_INFO_LOG("ReleaseSystemResourceManager success");
+}
+
+std::string ResourceManagerAdapter::GetSystemStringByName(std::string name)
+{
+    std::lock_guard<std::mutex> lock(resourceManagerMutex_);
+    InitResourceManager();
+
+    std::string result;
+    Global::Resource::RState rstate = resourceManager_->GetStringByName(name.c_str(), result);
+    AUDIO_INFO_LOG("name: %{public}s, rstate: %{public}d", name.c_str(), static_cast<int32_t>(rstate));
+    return result;
+}
+
+Global::Resource::RState ResourceManagerAdapter::GetMediaDataByName(std::string name, size_t& len,
+    std::unique_ptr<uint8_t[]> &outValue, uint32_t density)
+{
+    std::lock_guard<std::mutex> lock(resourceManagerMutex_);
+    InitResourceManager();
+
+    Global::Resource::RState = resourceManager_->GetMediaDataByName(name.c_str(), len, outValue);
+    iAUDIO_INFO_LOG("rstate: %{public}d", static_cast<int32_t>(rstate));
+    return rstate;
+}
+
+} // namespace AudioStandard
+} // namespace OHOS
