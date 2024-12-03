c71793e18b92b6c5372c0088a93a4ed4d29e221b
李立健 <lilijian5@h-partners.com>
2024-12-03 06:21:25 +0000

    检视意见修改
    Signed-off-by: 李立健 <lilijian5@h-partners.com>

--- frameworks/native/audioutils/src/audio_speed.cpp
+++ frameworks/native/audioutils/src/audio_speed.cpp
@@ -46,8 +46,8 @@ AudioSpeed::~AudioSpeed()
 int32_t AudioSpeed::Init()
 {
     sonicStream_ = sonicCreateStream(rate_, channels_);
-    LoadChangeSpeedFunc();
     CHECK_AND_RETURN_RET_LOG(sonicStream_ == nullptr, ERROR, "sonicCreateStream failed");
+    LoadChangeSpeedFunc();
 
     return SUCCESS;
 }
