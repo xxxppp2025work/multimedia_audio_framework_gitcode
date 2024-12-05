3449c116892eb2f4ffe483c6309fdc420c8ac78b
任军旗 <renjunqi3@huawei.com>
2024-12-05 08:01:44 +0000

    默认发声设备自恢复
    Signed-off-by: 任军旗 <renjunqi3@huawei.com>

--- services/audio_service/server/include/ipc_stream_stub.h
+++ services/audio_service/server/include/ipc_stream_stub.h
@@ -70,6 +70,8 @@ private:
 
     int32_t HandleRegisterThreadPriority(MessageParcel &data, MessageParcel &reply);
 
+    int32_t HandleSetDefaultOutputDevice(MessageParcel &data, MessageParcel &reply);
+
     int OnMiddleCodeRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option);
 };
 } // namespace AudioStandard
