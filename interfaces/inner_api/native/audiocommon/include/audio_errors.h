b0ae1af121a1f2d2f21390a067a31466fdc714dc
magekkkk <lixin113@huawei.com>
2024-12-06 11:29:48 +0000

    fix code review issues
    Signed-off-by: magekkkk <lixin113@huawei.com>

--- interfaces/inner_api/native/audiocommon/include/audio_errors.h
+++ interfaces/inner_api/native/audiocommon/include/audio_errors.h
@@ -156,7 +156,7 @@ const int32_t ERR_MMI_SUBSCRIBE = BASE_AUDIO_ERR_OFFSET - 31;
 /**multimode creation failed*/
 const int32_t ERR_MMI_CREATION = BASE_AUDIO_ERR_OFFSET - 32;
 /** Unknown error */
-const int32_t  ERR_UNKNOWN = BASE_AUDIO_ERR_OFFSET - 200;
+const int32_t ERR_UNKNOWN = BASE_AUDIO_ERR_OFFSET - 200;
 }  // namespace AudioStandard
 }  // namespace OHOS
 #endif  // AUDIO_ERRORS_H
