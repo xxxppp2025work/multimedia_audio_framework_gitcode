4aeccd89d7c680f8351aadc26a9abaad16f2311f
Gisaac <gongjie13@huawei.com>
2024-12-12 21:50:53 +0800

    rm code
    Signed-off-by: Gisaac <gongjie13@huawei.com>

--- services/audio_policy/server/src/service/routers/audio_router_center.cpp
+++ services/audio_policy/server/src/service/routers/audio_router_center.cpp
@@ -129,14 +129,6 @@ std::vector<std::shared_ptr<AudioDeviceDescriptor>> AudioRouterCenter::FetchOutp
         }
         if (audioScene == AUDIO_SCENE_PHONE_CALL || audioScene == AUDIO_SCENE_PHONE_CHAT ||
             ((audioScene == AUDIO_SCENE_RINGING || audioScene == AUDIO_SCENE_VOICE_RINGING) && HasScoDevice())) {
-            auto isPresent = [] (const unique_ptr<RouterBase> &router) {
-                return router->name_ == "package_filter_router";
-            };
-            auto itr = find_if(mediaRenderRouters_.begin(), mediaRenderRouters_.end(), isPresent);
-            if (itr != mediaRenderRouters_.end()) {
-                desc = (*itr)->GetMediaRenderDevice(streamUsage, clientUID);
-                routerType = (*itr)->GetRouterType();
-            }
             if (desc->deviceType_ == DEVICE_TYPE_NONE) {
                 StreamUsage callStreamUsage =
                     AudioStreamCollector::GetAudioStreamCollector().GetLastestRunningCallStreamUsage();
@@ -180,14 +172,6 @@ void AudioRouterCenter::DealRingRenderRouters(std::vector<std::shared_ptr<AudioD
     if (audioScene == AUDIO_SCENE_PHONE_CALL || audioScene == AUDIO_SCENE_PHONE_CHAT ||
         (audioScene == AUDIO_SCENE_VOICE_RINGING && isVoipStream)) {
         shared_ptr<AudioDeviceDescriptor> desc = make_shared<AudioDeviceDescriptor>();
-        auto isPresent = [] (const unique_ptr<RouterBase> &router) {
-            return router->name_ == "package_filter_router";
-        };
-        auto itr = find_if(mediaRenderRouters_.begin(), mediaRenderRouters_.end(), isPresent);
-        if (itr != mediaRenderRouters_.end()) {
-            desc = (*itr)->GetMediaRenderDevice(streamUsage, clientUID);
-            routerType = (*itr)->GetRouterType();
-        }
         if (desc->deviceType_ == DEVICE_TYPE_NONE) {
             AUDIO_INFO_LOG("Ring follow call strategy, replace usage %{public}d to %{public}d", streamUsage,
                 callStreamUsage);
