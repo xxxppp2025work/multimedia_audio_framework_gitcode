7c675691efc9f8d0bac4073d6469c2ad9f36fcd6
dlluhj <dailuhan@h-partners.com>
2024-12-19 02:46:02 +0000

    update services/audio_policy/server/src/service/service/audio_connected_device.cpp.
    Signed-off-by: dlluhj <dailuhan@h-partners.com>

--- services/audio_policy/server/src/service/service/audio_connected_device.cpp
+++ services/audio_policy/server/src/service/service/audio_connected_device.cpp
@@ -159,23 +159,17 @@ std::shared_ptr<AudioDeviceDescriptor> AudioConnectedDevice::GetConnectedDeviceB
 void AudioConnectedDevice::DelConnectedDevice(std::string networkId, DeviceType deviceType, std::string macAddress,
     DeviceRole deviceRole, bool isSingleDelete)
 {
+    auto isPresent = [&deviceType, &networkId, &macAddress,
+        &deviceRole] (const std::shared_ptr<AudioDeviceDescriptor> &descriptor) {
+        return descriptor->deviceType_ == deviceType && descriptor->networkId_ == networkId
+            && descriptor->macAddress_ == macAddress &&
+            (!IsUsb(descriptor->deviceType_) || descriptor->deviceRole_ == deviceRole);
+    };
     if (!isSingleDelete) {
-        auto isPresent = [&deviceType, &networkId, &macAddress,
-            &deviceRole] (const std::shared_ptr<AudioDeviceDescriptor> &descriptor) {
-            return descriptor->deviceType_ == deviceType && descriptor->networkId_ == networkId
-                && descriptor->macAddress_ == macAddress &&
-                (!IsUsb(descriptor->deviceType_) || descriptor->deviceRole_ == deviceRole);
-        };
         connectedDevices_.erase(std::remove_if(connectedDevices_.begin(), connectedDevices_.end(), isPresent),
             connectedDevices_.end());
         return;
     } else {
-        auto isPresent = [&deviceType, &networkId, &macAddress,
-            &deviceRole] (const std::shared_ptr<AudioDeviceDescriptor> &descriptor) {
-            return descriptor->deviceType_ == deviceType && descriptor->networkId_ == networkId
-                && descriptor->macAddress_ == macAddress &&
-                (!IsUsb(descriptor->deviceType_) || descriptor->deviceRole_ == deviceRole);
-        };
         auto it = std::find_if(connectedDevices_.begin(), connectedDevices_.end(), isPresent);
         if (it != connectedDevices_.end()) {
             connectedDevices_.erase(it);
