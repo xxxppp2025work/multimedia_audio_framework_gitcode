d3351653c895f17d9b92972e088b4c9e68f22ef1
Jvgang <yujugang1@h-partners.com>
2024-12-19 05:41:40 +0000

    ConvertToHDIAudioInputType 补充转录流映射Hdi流类型
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- frameworks/native/hdiadapter/source/primary/audio_capturer_source.cpp
+++ frameworks/native/hdiadapter/source/primary/audio_capturer_source.cpp
@@ -523,6 +523,7 @@ static enum AudioInputType ConvertToHDIAudioInputType(const int32_t currSourceTy
             hdiAudioInputType = AUDIO_INPUT_SPEECH_WAKEUP_TYPE;
             break;
         case SOURCE_TYPE_VOICE_COMMUNICATION:
+        case SOURCE_TYPE_VOICE_TRANSCRIPTION:
             hdiAudioInputType = AUDIO_INPUT_VOICE_COMMUNICATION_TYPE;
             break;
         case SOURCE_TYPE_VOICE_RECOGNITION:
