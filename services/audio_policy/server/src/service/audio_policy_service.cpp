7da08e9a09d69f567f4c5d53d810fc6f8234c759
Wang Hengshen <wanghengshen1@huawei.com>
2024-12-19 19:37:11 +0800

    refactor: audio device usage enums and fix typos in audio device management
    Signed-off-by: Wang Hengshen <wanghengshen1@huawei.com>

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -3725,12 +3725,12 @@ void AudioPolicyService::UpdateConnectedDevicesWhenConnectingForOutputDevice(
     DeviceUsage usage = GetDeviceUsage(updatedDesc);
     if (audioDescriptor->networkId_ == LOCAL_NETWORK_ID && audioDescriptor->isSameDeviceDesc(
         std::move(audioRouterCenter_.FetchOutputDevices(STREAM_USAGE_MEDIA, -1,
-        ROUTER_TYPE_USER_SELECT).front())) && (usage == MEDIA || usage == ALL_USAGE)) {
+        ROUTER_TYPE_USER_SELECT).front())) && (usage & MEDIA) == MEDIA) {
         SetPreferredDevice(AUDIO_MEDIA_RENDER, new(std::nothrow) AudioDeviceDescriptor());
     }
     if (audioDescriptor->networkId_ == LOCAL_NETWORK_ID && audioDescriptor->isSameDeviceDesc(
         std::move(audioRouterCenter_.FetchOutputDevices(STREAM_USAGE_VOICE_COMMUNICATION, -1,
-        ROUTER_TYPE_USER_SELECT).front())) && (usage == VOICE || usage == ALL_USAGE)) {
+        ROUTER_TYPE_USER_SELECT).front())) && (usage & VOICE) == VOICE) {
         SetPreferredDevice(AUDIO_CALL_RENDER, new(std::nothrow) AudioDeviceDescriptor());
     }
 }
