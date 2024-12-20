d2de0ca0488a04a5f9a176458ec1accb3d8e7025
s30056362 <shanliang6@huawei.com>
2024-12-20 09:44:59 +0800

    codecheck
    Signed-off-by: s30056362 <shanliang6@huawei.com>

--- frameworks/native/offlineaudioeffect/src/offline_audio_effect_server_chain.cpp
+++ frameworks/native/offlineaudioeffect/src/offline_audio_effect_server_chain.cpp
@@ -55,9 +55,9 @@ static inline void FreeIfNotNull(T*& ptr)
     }
 }
 
-static inline int32_t GetByteSize(AudioSampleFormat format)
+static inline uint32_t GetByteSize(AudioSampleFormat format)
 {
-    static const std::unordered_map<AudioSampleFormat, int32_t> sizeMap = {
+    static const std::unordered_map<AudioSampleFormat, uint32_t> sizeMap = {
         {SAMPLE_U8, 1},
         {SAMPLE_S16LE, 2},
         {SAMPLE_S24LE, 3},
@@ -160,8 +160,7 @@ int32_t OfflineAudioEffectServerChain::Create()
 
     std::lock_guard<std::mutex> lock(offlineChainMutex_);
     CHECK_AND_RETURN_RET_LOG(controller_, ERROR, "enable failed, controller is nullptr");
-    ret = controller_->SendCommand(controller_, AUDIO_EFFECT_COMMAND_ENABLE,
-        static_cast<int8_t *>(input), MAX_CMD_LEN, output, &replyLen);
+    ret = controller_->SendCommand(controller_, AUDIO_EFFECT_COMMAND_ENABLE, input, MAX_CMD_LEN, output, &replyLen);
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, ERROR,
         "%{public}s effect COMMAND_ENABLE failed, errCode is %{public}d", chainName_.c_str(), ret);
 
@@ -233,8 +232,8 @@ int32_t OfflineAudioEffectServerChain::Process(uint32_t inSize, uint32_t outSize
     struct AudioEffectBuffer input;
     struct AudioEffectBuffer output;
 
-    input = {inSize / GetFormatByteSize(offlineConfig_.inputCfg.format),
-        GetFormatByteSize(offlineConfig_.inputCfg.format),
+    input = {inSize / static_cast<uint32_t>(GetFormatByteSize(offlineConfig_.inputCfg.format)),
+        static_cast<uint32_t>(GetFormatByteSize(offlineConfig_.inputCfg.format)),
         reinterpret_cast<int8_t *>(serverBufferIn_->GetBase()), inSize};
     output = {};
 
@@ -242,8 +241,8 @@ int32_t OfflineAudioEffectServerChain::Process(uint32_t inSize, uint32_t outSize
     CHECK_AND_RETURN_RET_LOG(controller_, ERROR, "process failed, controller is nullptr");
     int32_t ret = controller_->EffectProcess(controller_, &input, &output);
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, ERROR, "EffectProcess failed ret:%{public}d", ret);
-    ret = memcpy_s(reinterpret_cast<int8_t *>(serverBufferOut_->GetBase()), outSize,
-        output.rawData, output.frameCount * GetFormatByteSize(offlineConfig_.outputCfg.format));
+    ret = memcpy_s(reinterpret_cast<int8_t *>(serverBufferOut_->GetBase()), outSize, output.rawData,
+        output.frameCount * static_cast<uint32_t>(GetFormatByteSize(offlineConfig_.outputCfg.format)));
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, ERROR, "memcpy failed, ret:%{public}d", ret);
     FreeIfNotNull(output.rawData);
 
