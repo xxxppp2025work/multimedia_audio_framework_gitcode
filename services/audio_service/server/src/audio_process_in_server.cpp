32413c9e5f2a89addacb96fa9e9605141cb9498c
Jvgang <yujugang1@h-partners.com>
2025-01-10 21:26:25 +0000

    Allow to start capturer stream when startUsingPermission Returns ERR_PERMISSION_ALREADY_START_USING !
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- services/audio_service/server/src/audio_process_in_server.cpp
+++ services/audio_service/server/src/audio_process_in_server.cpp
@@ -131,12 +131,12 @@ int32_t AudioProcessInServer::Start()
     CHECK_AND_RETURN_RET_LOG(streamStatus_->load() == STREAM_STARTING || streamStatus_->load() == STREAM_STAND_BY,
         ERR_ILLEGAL_STATE, "Start failed, invalid status.");
 
-    if (processConfig_.audioMode != AUDIO_MODE_PLAYBACK && !needCheckBackground_ &&
+    if (processConfig_.audioMode == AUDIO_MODE_RECORD && !needCheckBackground_ &&
         PermissionUtil::NeedVerifyBackgroundCapture(processConfig_.callerUid, processConfig_.capturerInfo.sourceType)) {
         AUDIO_INFO_LOG("set needCheckBackground_: true");
         needCheckBackground_ = true;
     }
-    if (processConfig_.audioMode != AUDIO_MODE_PLAYBACK && needCheckBackground_) {
+    if (processConfig_.audioMode == AUDIO_MODE_RECORD && needCheckBackground_) {
         CHECK_AND_RETURN_RET_LOG(PermissionUtil::VerifyBackgroundCapture(processConfig_.appInfo.appTokenId,
             processConfig_.appInfo.appFullTokenId), ERR_OPERATION_FAILED, "VerifyBackgroundCapture failed!");
         CHECK_AND_RETURN_RET_LOG(PermissionUtil::NotifyStart(processConfig_.appInfo.appTokenId, sessionId_),
@@ -166,7 +166,7 @@ int32_t AudioProcessInServer::Pause(bool isFlush)
     std::lock_guard<std::mutex> lock(statusLock_);
     CHECK_AND_RETURN_RET_LOG(streamStatus_->load() == STREAM_PAUSING,
         ERR_ILLEGAL_STATE, "Pause failed, invalid status.");
-    if (processConfig_.audioMode != AUDIO_MODE_PLAYBACK && needCheckBackground_) {
+    if (processConfig_.audioMode == AUDIO_MODE_RECORD && needCheckBackground_) {
         uint32_t tokenId = processConfig_.appInfo.appTokenId;
         PermissionUtil::NotifyStop(tokenId, sessionId_);
     }
@@ -184,12 +184,12 @@ int32_t AudioProcessInServer::Resume()
     std::lock_guard<std::mutex> lock(statusLock_);
     CHECK_AND_RETURN_RET_LOG(streamStatus_->load() == STREAM_STARTING,
         ERR_ILLEGAL_STATE, "Resume failed, invalid status.");
-    if (processConfig_.audioMode != AUDIO_MODE_PLAYBACK && !needCheckBackground_ &&
+    if (processConfig_.audioMode == AUDIO_MODE_RECORD && !needCheckBackground_ &&
         PermissionUtil::NeedVerifyBackgroundCapture(processConfig_.callerUid, processConfig_.capturerInfo.sourceType)) {
         AUDIO_INFO_LOG("set needCheckBackground_: true");
         needCheckBackground_ = true;
     }
-    if (processConfig_.audioMode != AUDIO_MODE_PLAYBACK && needCheckBackground_) {
+    if (processConfig_.audioMode == AUDIO_MODE_RECORD && needCheckBackground_) {
         uint32_t tokenId = processConfig_.appInfo.appTokenId;
         uint64_t fullTokenId = processConfig_.appInfo.appFullTokenId;
         CHECK_AND_RETURN_RET_LOG(PermissionUtil::VerifyBackgroundCapture(tokenId, fullTokenId), ERR_OPERATION_FAILED,
@@ -213,7 +213,7 @@ int32_t AudioProcessInServer::Stop()
     std::lock_guard<std::mutex> lock(statusLock_);
     CHECK_AND_RETURN_RET_LOG(streamStatus_->load() == STREAM_STOPPING,
         ERR_ILLEGAL_STATE, "Stop failed, invalid status.");
-    if (processConfig_.audioMode != AUDIO_MODE_PLAYBACK && needCheckBackground_) {
+    if (processConfig_.audioMode == AUDIO_MODE_RECORD && needCheckBackground_) {
         uint32_t tokenId = processConfig_.appInfo.appTokenId;
         PermissionUtil::NotifyStop(tokenId, sessionId_);
     }
@@ -234,7 +234,7 @@ int32_t AudioProcessInServer::Release(bool isSwitchStream)
     std::lock_guard<std::mutex> lock(statusLock_);
     CHECK_AND_RETURN_RET_LOG(releaseCallback_ != nullptr, ERR_OPERATION_FAILED, "Failed: no service to notify.");
 
-    if (processConfig_.audioMode != AUDIO_MODE_PLAYBACK && needCheckBackground_) {
+    if (processConfig_.audioMode == AUDIO_MODE_RECORD && needCheckBackground_) {
         uint32_t tokenId = processConfig_.appInfo.appTokenId;
         PermissionUtil::NotifyStop(tokenId, sessionId_);
     }
