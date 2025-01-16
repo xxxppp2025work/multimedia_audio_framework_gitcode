84ce5e50460e41e8974768ee28fac7b6f42aaa69
zhangwenting3652 <zhangwenting15@huawei.com>
2025-01-16 20:05:31 +0800

    添加feature化编译-不支持低时延
    Signed-off-by: zhangwenting3652 <zhangwenting15@huawei.com>

--- services/audio_service/server/include/audio_service.h
+++ services/audio_service/server/include/audio_service.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,8 +23,15 @@
 #include <mutex>
 #include <vector>
 
+#ifdef SUPPORT_LOW_LATENCY
 #include "audio_process_in_server.h"
 #include "audio_endpoint.h"
+#endif
+
+#include "i_audio_process_stream.h"
+#include "i_audio_process.h"
+#include "audio_info.h"
+#include "audio_device_descriptor.h"
 #include "ipc_stream_in_server.h"
 #include "playback_capturer_manager.h"
 
@@ -38,7 +45,12 @@ enum InnerCapFilterPolicy : uint32_t {
 };
 } // anonymous namespace
 
-class AudioService : public ProcessReleaseCallback, public ICapturerFilterListener {
+#ifdef SUPPORT_LOW_LATENCY
+class AudioService : public ProcessReleaseCallback, public ICapturerFilterListener
+#else
+class AudioService : public ICapturerFilterListener
+#endif
+{
 public:
     static AudioService *GetInstance();
     ~AudioService();
@@ -48,7 +60,9 @@ public:
     int32_t OnCapturerFilterRemove(uint32_t sessionId) override;
 
     sptr<IpcStreamInServer> GetIpcStream(const AudioProcessConfig &config, int32_t &ret);
+    int32_t NotifyStreamVolumeChanged(AudioStreamType streamType, float volume);
 
+#ifdef SUPPORT_LOW_LATENCY
     sptr<AudioProcessInServer> GetAudioProcess(const AudioProcessConfig &config);
     // override for ProcessReleaseCallback, do release process work.
     int32_t OnProcessRelease(IAudioProcessStream *process, bool isSwitchStream = false) override;
@@ -57,13 +71,14 @@ public:
     AudioDeviceDescriptor GetDeviceInfoForProcess(const AudioProcessConfig &config);
     std::shared_ptr<AudioEndpoint> GetAudioEndpointForDevice(AudioDeviceDescriptor &deviceInfo,
         const AudioProcessConfig &clientConfig, bool isVoipStream);
-    int32_t NotifyStreamVolumeChanged(AudioStreamType streamType, float volume);
 
     int32_t LinkProcessToEndpoint(sptr<AudioProcessInServer> process, std::shared_ptr<AudioEndpoint> endpoint);
     int32_t UnlinkProcessToEndpoint(sptr<AudioProcessInServer> process, std::shared_ptr<AudioEndpoint> endpoint);
+    void ResetAudioEndpoint();
+#endif
+
     void Dump(std::string &dumpString);
     float GetMaxAmplitude(bool isOutputDevice);
-    void ResetAudioEndpoint();
 
     void RemoveRenderer(uint32_t sessionId);
     void RemoveCapturer(uint32_t sessionId);
@@ -94,8 +109,13 @@ private:
     void InsertCapturer(uint32_t sessionId, std::shared_ptr<CapturerInServer> capturer);
     // for inner-capturer
     void CheckInnerCapForRenderer(uint32_t sessionId, std::shared_ptr<RendererInServer> renderer);
+#ifdef SUPPORT_LOW_LATENCY
     void CheckInnerCapForProcess(sptr<AudioProcessInServer> process, std::shared_ptr<AudioEndpoint> endpoint);
     void FilterAllFastProcess();
+
+    void CheckFastSessionMuteState(uint32_t sessionId, sptr<AudioProcessInServer> process);
+    int32_t GetReleaseDelayTime(std::shared_ptr<AudioEndpoint> endpoint, bool isSwitchStream);
+#endif
     InnerCapFilterPolicy GetInnerCapFilterPolicy();
     bool ShouldBeInnerCap(const AudioProcessConfig &rendererConfig);
     bool ShouldBeDualTone(const AudioProcessConfig &config);
@@ -105,18 +125,18 @@ private:
     void RemoveIdFromMuteControlSet(uint32_t sessionId);
     void CheckRenderSessionMuteState(uint32_t sessionId, std::shared_ptr<RendererInServer> renderer);
     void CheckCaptureSessionMuteState(uint32_t sessionId, std::shared_ptr<CapturerInServer> capturer);
-    void CheckFastSessionMuteState(uint32_t sessionId, sptr<AudioProcessInServer> process);
-    int32_t GetReleaseDelayTime(std::shared_ptr<AudioEndpoint> endpoint, bool isSwitchStream);
     void ReLinkProcessToEndpoint();
 
 private:
     std::mutex processListMutex_;
-    std::vector<std::pair<sptr<AudioProcessInServer>, std::shared_ptr<AudioEndpoint>>> linkedPairedList_;
-
     std::mutex releaseEndpointMutex_;
     std::condition_variable releaseEndpointCV_;
     std::set<std::string> releasingEndpointSet_;
+
+#ifdef SUPPORT_LOW_LATENCY
+    std::vector<std::pair<sptr<AudioProcessInServer>, std::shared_ptr<AudioEndpoint>>> linkedPairedList_;
     std::map<std::string, std::shared_ptr<AudioEndpoint>> endpointList_;
+#endif
 
     // for inner-capturer
     PlaybackCapturerManager *innerCapturerMgr_ = nullptr;
