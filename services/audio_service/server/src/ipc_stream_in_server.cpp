e9f3761f924db32e386d9a93f00a565c6cedc0a1
wuyajun <wuyajun11@h-partners.com>
2025-01-17 11:41:37 +0000

    客户端优先级调整
    Signed-off-by: wuyajun <wuyajun11@h-partners.com>

--- services/audio_service/server/src/ipc_stream_in_server.cpp
+++ services/audio_service/server/src/ipc_stream_in_server.cpp
@@ -224,7 +224,7 @@ int32_t IpcStreamInServer::Stop()
 
 int32_t IpcStreamInServer::Release()
 {
-    UnscheduleReportData(config_.appInfo.appPid, clientTid_, clientBundleName_.c_str());
+    UnscheduleReportData(IPCSkeleton::GetCallingPid(), clientTid_, clientBundleName_.c_str());
     clientThreadPriorityRequested_ = false;
     if (mode_ == AUDIO_MODE_PLAYBACK && rendererInServer_ != nullptr) {
         return rendererInServer_->Release();
@@ -464,7 +464,7 @@ int32_t IpcStreamInServer::RegisterThreadPriority(uint32_t tid, const std::strin
     if (!clientThreadPriorityRequested_) {
         clientTid_ = tid;
         clientBundleName_ = bundleName;
-        ScheduleReportData(config_.appInfo.appPid, tid, bundleName.c_str());
+        ScheduleReportData(IPCSkeleton::GetCallingPid(), tid, bundleName.c_str());
         return SUCCESS;
     } else {
         AUDIO_ERR_LOG("client thread priority requested");
