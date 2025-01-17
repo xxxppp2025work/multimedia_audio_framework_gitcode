e26bd82a7cb594ab07805ed3828ce3f2f0abf63d
wuyajun <wuyajun11@h-partners.com>
2025-01-17 12:55:32 +0000

    客户端优先级调整
    Signed-off-by: wuyajun <wuyajun11@h-partners.com>

--- services/audio_service/server/src/ipc_stream_in_server.cpp
+++ services/audio_service/server/src/ipc_stream_in_server.cpp
@@ -23,6 +23,7 @@
 #include "audio_service_log.h"
 #include "audio_errors.h"
 #include "audio_schedule.h"
+#include "ipc_skeleton.h"
 
 namespace OHOS {
 namespace AudioStandard {
@@ -224,7 +225,8 @@ int32_t IpcStreamInServer::Stop()
 
 int32_t IpcStreamInServer::Release()
 {
-    UnscheduleReportData(config_.appInfo.appPid, clientTid_, clientBundleName_.c_str());
+    clientPid_ = IPCSkeleton::GetCallingPid();
+    UnscheduleReportData(clientPid_, clientTid_, clientBundleName_.c_str());
     clientThreadPriorityRequested_ = false;
     if (mode_ == AUDIO_MODE_PLAYBACK && rendererInServer_ != nullptr) {
         return rendererInServer_->Release();
@@ -462,9 +464,10 @@ int32_t IpcStreamInServer::SetDuckFactor(float duckFactor)
 int32_t IpcStreamInServer::RegisterThreadPriority(uint32_t tid, const std::string &bundleName)
 {
     if (!clientThreadPriorityRequested_) {
+        clientPid_ = IPCSkeleton::GetCallingPid();
         clientTid_ = tid;
         clientBundleName_ = bundleName;
-        ScheduleReportData(config_.appInfo.appPid, tid, bundleName.c_str());
+        ScheduleReportData(clientPid_, tid, bundleName.c_str());
         return SUCCESS;
     } else {
         AUDIO_ERR_LOG("client thread priority requested");
