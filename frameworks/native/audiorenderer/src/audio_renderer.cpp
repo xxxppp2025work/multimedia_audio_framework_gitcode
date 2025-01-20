44d47ec4f92bbd5fa999fd12b66c31ecb8c1c938
heyabinghyb <heyabing@h-partners.com>
2025-01-20 13:15:42 +0000

    删除多余代码
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- frameworks/native/audiorenderer/src/audio_renderer.cpp
+++ frameworks/native/audiorenderer/src/audio_renderer.cpp
@@ -2001,7 +2001,6 @@ void RendererPolicyServiceDiedCallback::RestoreTheadLoop()
     int32_t tryCounter = 10;
     uint32_t sleepTime = 300000;
     bool restoreResult = false;
-    OHOS::AudioStandard::ObjectRefMap objectGuard(renderer_);
     while (!restoreResult && tryCounter > 0) {
         tryCounter--;
         usleep(sleepTime);
