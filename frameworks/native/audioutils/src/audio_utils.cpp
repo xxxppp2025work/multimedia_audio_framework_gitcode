23205dfbbcbdf49c85f0760f156cd43ac1078675
Jvgang <yujugang1@h-partners.com>
2025-01-25 06:58:12 +0000

    格式调整
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- frameworks/native/audioutils/src/audio_utils.cpp
+++ frameworks/native/audioutils/src/audio_utils.cpp
@@ -484,7 +484,7 @@ bool SwitchStreamUtil::isSwitchStreamSwtching(SwitchStreamInfo info, SwitchState
         return true;
         }
     if(iter != g_switchStreamRecordMap.end() && targetState == SWITCH_STATE_STARTED
-    && iter->second == SWITCH_STATE_CREATED && (info.nextState == CAPTURER_RUNNING)){
+        && iter->second == SWITCH_STATE_CREATED && (info.nextState == CAPTURER_RUNNING)){
         AUDIO_WARNING_LOG("stream:%{public}u is restarting , need not check using mic in background !",
         info.sessionId);
         return true;
