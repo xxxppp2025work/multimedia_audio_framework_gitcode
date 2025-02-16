602b5cf0f89ffb2e0c3ddb92bf0312cf59100b5d
buertianshi <yuelei19@huawei.com>
2025-02-16 11:28:47 +0000

    clear workspace
    Signed-off-by: buertianshi <yuelei19@huawei.com>

--- services/audio_policy/test/unittest/audio_router_center_unit_test/include/audio_router_center_unit_test.h
+++ services/audio_policy/test/unittest/audio_router_center_unit_test/include/audio_router_center_unit_test.h
@@ -13,28 +13,28 @@
  * limitations under the License.
  */
 
- #ifndef AUDIO_ROUTER_CENTER_UNIT_TEST_H
- #define AUDIO_ROUTER_CENTER_UNIT_TEST_H
+#ifndef AUDIO_ROUTER_CENTER_UNIT_TEST_H
+#define AUDIO_ROUTER_CENTER_UNIT_TEST_H
  
- #include "gtest/gtest.h"
- #include "audio_router_center.h"
- #include "audio_system_manager.h"
- #include "audio_policy_manager_factory.h"
+#include "gtest/gtest.h"
+#include "audio_router_center.h"
+#include "audio_system_manager.h"
+#include "audio_policy_manager_factory.h"
  
- namespace OHOS {
- namespace AudioStandard {
+namespace OHOS {
+namespace AudioStandard {
  
- class AudioRouterCenterUnitTest : public testing::Test {
- public:
-     // SetUpTestCase: Called before all test cases
-     static void SetUpTestCase(void);
-     // TearDownTestCase: Called after all test case
-     static void TearDownTestCase(void);
-     // SetUp: Called before each test cases
-     void SetUp(void);
-     // TearDown: Called after each test cases
-     void TearDown(void);
- };
- } // namespace AudioStandard
- } // namespace OHOS
- #endif // AUDIO_ROUTER_CENTER_UNIT_TEST_H
+class AudioRouterCenterUnitTest : public testing::Test {
+public:
+    // SetUpTestCase: Called before all test cases
+    static void SetUpTestCase(void);
+    // TearDownTestCase: Called after all test case
+    static void TearDownTestCase(void);
+    // SetUp: Called before each test cases
+    void SetUp(void);
+    // TearDown: Called after each test cases
+    void TearDown(void);
+};
+} // namespace AudioStandard
+} // namespace OHOS
+#endif // AUDIO_ROUTER_CENTER_UNIT_TEST_H
