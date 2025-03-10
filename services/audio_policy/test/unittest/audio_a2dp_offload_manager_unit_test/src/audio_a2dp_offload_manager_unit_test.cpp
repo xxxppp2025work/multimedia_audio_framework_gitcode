850f7edf3c034c9395e34edcd7f3a2bcdc49806e
liujia178 <liujia178@huawei.com>
2025-03-10 17:52:41 +0800

    0310_tdd
    Signed-off-by: liujia178 <liujia178@huawei.com>

--- services/audio_policy/test/unittest/audio_a2dp_offload_manager_unit_test/src/audio_a2dp_offload_manager_unit_test.cpp
+++ services/audio_policy/test/unittest/audio_a2dp_offload_manager_unit_test/src/audio_a2dp_offload_manager_unit_test.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "audio_policy_utils.h"
 #include "audio_a2dp_offload_manager_unit_test.h"
 
@@ -212,7 +227,8 @@ HWTEST_F(AudioA2dpOffloadManagerUnitTest, UpdateA2dpOffloadFlagForAllStream_002,
 
 /**
  * @tc.name: UpdateA2dpOffloadFlag_001
- * @tc.desc: Test UpdateA2dpOffloadFlag with deviceType DEVICE_TYPE_BLUETOOTH_A2DP and receiveOffloadFlag != GetA2dpOffloadFlag.
+ * @tc.desc: Test UpdateA2dpOffloadFlag with deviceType DEVICE_TYPE_BLUETOOTH_A2DP
+ *  and receiveOffloadFlag != GetA2dpOffloadFlag.
  * @tc.type: FUNC
  * @tc.require: #I5Y4MZ
  */
