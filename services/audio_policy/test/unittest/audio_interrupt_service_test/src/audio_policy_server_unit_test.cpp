dbdeb2283a7f7ea1bf49804544d52ff995e3b69e
zhangwenting15 <zhangwenting15@huawei.com>
2025-03-18 11:39:54 +0800

    音量及响铃Beta问题DFX增强
    Signed-off-by: zhangwenting15 <zhangwenting15@huawei.com>

--- services/audio_policy/test/unittest/audio_interrupt_service_test/src/audio_policy_server_unit_test.cpp
+++ services/audio_policy/test/unittest/audio_interrupt_service_test/src/audio_policy_server_unit_test.cpp
@@ -1453,5 +1453,39 @@ HWTEST(AudioPolicyUnitTest, MicrophoneMuteInfoDump_001, TestSize.Level1)
     std::string dumpString = "";
     ptrAudioPolicyServer->MicrophoneMuteInfoDump(dumpString);
 }
+
+/**
+* @tc.name  : Test AudioPolicyServer.
+* @tc.number: AudioPolicyServer_046
+* @tc.desc  : Test AudioPolicyServer::SetRingerMode
+*/
+HWTEST(AudioPolicyUnitTest, AudioPolicyServer_046, TestSize.Level1)
+{
+    int32_t systemAbilityId = 3009;
+    bool runOnCreate = false;
+    auto ptrAudioPolicyServer = std::make_shared<AudioPolicyServer>(systemAbilityId, runOnCreate);
+    EXPECT_NE(ptrAudioPolicyServer, nullptr);
+
+    ptrAudioPolicyServer->coreService_ = std::make_shared<AudioCoreService>();
+    auto ret = ptrAudioPolicyServer->SetRingerMode(AudioRingerMode::RINGER_MODE_NORMAL);
+    EXPECT_EQ(ret, SUCCESS);
+}
+
+/**
+* @tc.name  : Test AudioPolicyServer.
+* @tc.number: AudioPolicyServer_047
+* @tc.desc  : Test ArgInfoDump.
+*/
+HWTEST(AudioPolicyUnitTest, AudioPolicyServer_047, TestSize.Level1)
+{
+    int32_t systemAbilityId = 3009;
+    bool runOnCreate = false;
+    sptr<AudioPolicyServer> server = sptr<AudioPolicyServer>::MakeSptr(systemAbilityId, runOnCreate);
+    ASSERT_TRUE(server != nullptr);
+
+    std::string dumpString;
+    std::queue<std::u16string> argQue;
+    server->ArgInfoDump(dumpString, argQue);
+}
 } // AudioStandard
 } // OHOS
