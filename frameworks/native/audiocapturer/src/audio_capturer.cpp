c2017e11f0bf0d78bd71705620eb96839c20280f
tianmuhao <tianmuhao@h-partners.com>
2025-04-03 06:24:20 +0000

    update frameworks/native/audiocapturer/src/audio_capturer.cpp.
    Signed-off-by: tianmuhao <tianmuhao@h-partners.com>

--- frameworks/native/audiocapturer/src/audio_capturer.cpp
+++ frameworks/native/audiocapturer/src/audio_capturer.cpp
@@ -753,14 +753,14 @@ bool AudioCapturerPrivate::GetAudioTime(Timestamp &timestamp, Timestamp::Timesta
 const uint32_t BASE_TEN = 10;
 bool AudioCapturerPrivate::GetFirstPkgTimeStampInfo(long &firstTs) const
 {
-    AUDIO_INFO_LOG("[DJF]StreamClient for Capturer::Get first pkg timestamp info.");
+    AUDIO_INFO_LOG("StreamClient for Capturer::Get first pkg timestamp info.");
     const std::string SUBKEY_latency = "record_algo_first_ts";
     std::vector<std::pair<std::string, std::string>> results;
 
     int32_t ret =
         AudioSystemManager::GetInstance()->GetExtraParameters("audio_effect", { SUBKEY_latency }, results);
     if (ret != 0) {
-        AUDIO_WARNING_LOG("[DJF]AudioCapturerPrivate GetExtraParameters fail! %{public}d", ret);
+        AUDIO_WARNING_LOG("AudioCapturerPrivate GetExtraParameters fail! %{public}d", ret);
         return false;
     }
 
@@ -770,12 +770,12 @@ bool AudioCapturerPrivate::GetFirstPkgTimeStampInfo(long &firstTs) const
         });
     if (iter == results.end() || iter->second.empty()) {
         AUDIO_WARNING_LOG(
-            "[DJF]AudioCapturerPrivate GetExtraParameters fail! cannot find result or is empty string");
+            "AudioCapturerPrivate GetExtraParameters fail! cannot find result or is empty string");
         return false;
     }
 
     firstTs = std::strtol(iter->second.c_str(), nullptr, BASE_TEN);
-    AUDIO_INFO_LOG("[DJF]StreamClient for Capturer::first ts is raw %{public}s and value %{public}ld",
+    AUDIO_INFO_LOG("StreamClient for Capturer::first ts is raw %{public}s and value %{public}ld",
         iter->second.c_str(), firstTs);
     return true;
 }
