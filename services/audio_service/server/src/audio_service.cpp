60ab5901ab4aca21910621b590d8567756cc257c
ranranshengqi1999 <liran45@huawei.com>
2025-04-16 16:01:47 +0800

    modify max num
    Signed-off-by: ranranshengqi1999 <liran45@huawei.com>

--- services/audio_service/server/src/audio_service.cpp
+++ services/audio_service/server/src/audio_service.cpp
@@ -28,6 +28,7 @@
 #include "ipc_stream_in_server.h"
 #include "audio_capturer_source.h"
 #include "audio_volume.h"
+#include "media_monitor_manager.h"
 
 namespace OHOS {
 namespace AudioStandard {
@@ -38,6 +39,9 @@ static const uint32_t A2DP_ENDPOINT_RELEASE_DELAY_TIME = 3000; // 3ms
 static const uint32_t VOIP_ENDPOINT_RELEASE_DELAY_TIME = 200; // 200ms
 static const uint32_t A2DP_ENDPOINT_RE_CREATE_RELEASE_DELAY_TIME = 200; // 200ms
 static const int32_t MEDIA_SERVICE_UID = 1013;
+static const int32_t RENDERER_STREAM_CNT_PER_UID_LIMIT = 40;
+static const int32_t INVALID_APP_UID = -1;
+static const int32_t INVALID_APP_CREATED_AUDIO_STREAM_NUM = 0;
 
 AudioService *AudioService::GetInstance()
 {
@@ -1036,6 +1040,19 @@ bool AudioService::IsExceedingMaxStreamCntPerUid(int32_t callingUid, int32_t app
     } else {
         int32_t initValue = 1;
         appUseNumMap_.emplace(appUid, initValue);
+    }
+
+    if (appUseNumMap_[appUid] >= RENDERER_STREAM_CNT_PER_UID_LIMIT) {
+        int32_t mostAppUid = INVALID_APP_UID;
+        int32_t mostAppNum = INVALID_APP_CREATED_AUDIO_STREAM_NUM;
+        GetCreatedAudioStreamMostUid(mostAppUid, mostAppNum);
+        std::shared_ptr<Media::MediaMonitor::EventBean> bean = std::make_shared<Media::MediaMonitor::EventBean>(
+            Media::MediaMonitor::ModuleId::AUDIO, Media::MediaMonitor::EventId::AUDIO_STREAM_EXHAUSTED_STATS,
+            Media::MediaMonitor::EventType::FREQUENCY_AGGREGATION_EVENT);
+        bean->Add("CLIENT_UID", mostAppUid);
+        bean->Add("TIMES", mostAppNum);
+        Media::MediaMonitor::MediaMonitorManager::GetInstance().WriteLogMsg(bean);
+        AUDIO_WARNING_LOG("Current audio renderer stream num is greater than the renderer stream num limit per uid");
     }
 
     if (appUseNumMap_[appUid] > maxStreamCntPerUid) {
