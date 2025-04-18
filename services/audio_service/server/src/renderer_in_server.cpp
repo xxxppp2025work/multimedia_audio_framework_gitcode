87515b8a88d1b1a3504309c572a3100bc2af15ce
darkshadow <chenyan218@huawei.com>
2025-04-18 19:21:54 +0800

    eac3 数据透传
    Signed-off-by: darkshadow <chenyan218@huawei.com>

--- services/audio_service/server/src/renderer_in_server.cpp
+++ services/audio_service/server/src/renderer_in_server.cpp
@@ -149,9 +149,12 @@ int32_t RendererInServer::Init()
     if (IsHighResolution()) {
         Trace trace("current stream marked as high resolution");
         managerType_ = DIRECT_PLAYBACK;
-        AUDIO_INFO_LOG("current stream marked as high resolution");
+        AUDIO_INFO_LOG("current stream maked as high resolution");
     }
-
+    if (processConfig_.streamInfo.encoding == ENCODING_EAC3) {
+        managerType_ = EAC3_PLAYBACK;
+        AUDIO_INFO_LOG("current stream marked as eac3 direct stream");
+    }
     if (processConfig_.rendererInfo.rendererFlags == AUDIO_FLAG_VOIP_DIRECT) {
         if (IStreamManager::GetPlaybackManager(VOIP_PLAYBACK).GetStreamCount() <= 0) {
             AUDIO_INFO_LOG("current stream marked as VoIP direct stream");
@@ -778,7 +781,7 @@ int32_t RendererInServer::StartInner()
     fadeLock.unlock();
     ret = CoreServiceHandler::GetInstance().UpdateSessionOperation(streamIndex_, SESSION_OPERATION_START);
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, ret, "Policy start client failed, reason: %{public}d", ret);
-    ret = (managerType_ == DIRECT_PLAYBACK || managerType_ == VOIP_PLAYBACK) ?
+    ret = (managerType_ == DIRECT_PLAYBACK || managerType_ == VOIP_PLAYBACK || managerType_ == EAC3_PLAYBACK) ?
         IStreamManager::GetPlaybackManager(managerType_).StartRender(streamIndex_) : stream_->Start();
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, ret, "Start stream failed, reason: %{public}d", ret);
 
@@ -841,7 +844,7 @@ int32_t RendererInServer::Pause()
         }
     }
     standByCounter_ = 0;
-    int32_t ret = (managerType_ == DIRECT_PLAYBACK || managerType_ == VOIP_PLAYBACK) ?
+    int32_t ret = (managerType_ == DIRECT_PLAYBACK || managerType_ == VOIP_PLAYBACK || managerType_ == EAC3_PLAYBACK) ?
         IStreamManager::GetPlaybackManager(managerType_).PauseRender(streamIndex_) : stream_->Pause();
     {
         std::lock_guard<std::mutex> lock(dupMutex_);
