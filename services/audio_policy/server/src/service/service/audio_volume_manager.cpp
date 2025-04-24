29c2c2dd0a45d312b5d7496c5b53654fe7a092f2
zhangwenting <zhangwenting15@huawei.com>
2025-04-24 10:04:25 +0800

    修复安规异常音量设置问题
    Signed-off-by: zhangwenting <zhangwenting15@huawei.com>

--- services/audio_policy/server/src/service/service/audio_volume_manager.cpp
+++ services/audio_policy/server/src/service/service/audio_volume_manager.cpp
@@ -384,10 +384,15 @@ int32_t AudioVolumeManager::SetSystemVolumeLevel(AudioStreamType streamType, int
     }
     int32_t sVolumeLevel = SelectDealSafeVolume(streamType, volumeLevel);
     CheckToCloseNotification(streamType, volumeLevel);
-    CHECK_AND_RETURN_RET_LOG(sVolumeLevel == volumeLevel, ERR_SET_VOL_FAILED_BY_SAFE_VOL,
-        "safevolume did not deal");
-    result = audioPolicyManager_.SetSystemVolumeLevel(VolumeUtils::GetVolumeTypeFromStreamType(streamType),
-        volumeLevel);
+    if (sVolumeLevel != volumeLevel) {
+        result = audioPolicyManager_.SetSystemVolumeLevel(VolumeUtils::GetVolumeTypeFromStreamType(streamType),
+            sVolumeLevel);
+        AUDIO_INFO_LOG("sVolumeLevel : %{public}d, safevolume did not deal", sVolumeLevel);
+    } else {
+        result = audioPolicyManager_.SetSystemVolumeLevel(VolumeUtils::GetVolumeTypeFromStreamType(streamType),
+            volumeLevel);
+    }
+
     if (result == SUCCESS && (streamType == STREAM_VOICE_CALL || streamType == STREAM_VOICE_COMMUNICATION)) {
         SetVoiceCallVolume(volumeLevel);
     }
