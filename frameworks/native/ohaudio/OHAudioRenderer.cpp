404ad159f87214ad43f1caafeda4ed4b0af76e52
heyabinghyb <heyabing@h-partners.com>
2025-04-25 10:52:22 +0800

    删除audiorender置空
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- frameworks/native/ohaudio/OHAudioRenderer.cpp
+++ frameworks/native/ohaudio/OHAudioRenderer.cpp
@@ -464,7 +464,6 @@ bool OHAudioRenderer::Release()
     if (!audioRenderer_->Release()) {
         return false;
     }
-    audioRenderer_ = nullptr;
     audioRendererCallback_ = nullptr;
     return true;
 }
