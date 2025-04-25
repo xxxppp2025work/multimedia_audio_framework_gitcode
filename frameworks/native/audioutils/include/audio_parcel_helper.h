6ea4bd7d114a97970a00a1460274f08afd733603
kw <wangyuqi30@huawei.com>
2025-04-25 14:20:18 +0800

    AudioParcelHelper std::optional
    Signed-off-by: kw <wangyuqi30@huawei.com>

--- frameworks/native/audioutils/include/audio_parcel_helper.h
+++ frameworks/native/audioutils/include/audio_parcel_helper.h
@@ -16,6 +16,7 @@
 #define AUDIO_PARCEL_HELPER_H
 
 #include <cinttypes>
+#include <optional>
 
 namespace OHOS {
 namespace AudioStandard {
@@ -80,6 +81,30 @@ public:
     static uint64_t Unmarshalling(Parcelable &parcel)
     {
         return parcel.ReadUInt64();
+    }
+};
+
+template<typename Parcelable, typename T>
+class AudioParcelHelper<Parcelable, std::optional<T>> {
+public:
+    static bool Marshalling(Parcelable &parcel, const std::optional<T> &t)
+    {
+        bool hasValue = t.has_value();
+        parcel.WriteBool(hasValue);
+        if (!hasValue) {
+            return true;
+        } else {
+            return AudioParcelHelper<Parcelable, T>::Marshalling(t.value());
+        }
+    }
+
+    static std::optional<T> Unmarshalling(Parcelable &parcel)
+    {
+        if (!parcel.ReadBool()) {
+            return std::nullopt;
+        } else {
+            return AudioParcelHelper<Parcelable, T>::Unmarshalling();
+        }
     }
 };
 } // namespace AudioStandard
