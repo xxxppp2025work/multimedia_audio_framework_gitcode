93b30ab2543b989b3472fbebe9c6abb1741939b3
zhangke <zhangke173@huawei.com>
2025-04-26 17:56:00 +0800

    fix the clock system tone
    Signed-off-by: zhangke <zhangke173@huawei.com>

--- services/audio_service/server/src/audio_endpoint.cpp
+++ services/audio_service/server/src/audio_endpoint.cpp
@@ -1320,7 +1320,7 @@ void AudioEndpointInner::GetAllReadyProcessData(std::vector<AudioStreamData> &au
         DeviceType deviceType = PolicyHandler::GetInstance().GetActiveOutPutDevice();
         bool muteFlag = processList_[i]->GetMuteState();
         bool getVolumeRet = PolicyHandler::GetInstance().GetSharedVolume(volumeType, deviceType, vol);
-        int32_t doNotDisturbStatusVolume = AudioVolume::GetInstance()->GetDoNotDisturbStatusVolume(volumeType,
+        int32_t doNotDisturbStatusVolume = AudioVolume::GetInstance()->GetDoNotDisturbStatusVolume(streamType,
             clientConfig_.appInfo.appUid);
         if (deviceInfo_.networkId_ == LOCAL_NETWORK_ID &&
             !(deviceInfo_.deviceType_ == DEVICE_TYPE_BLUETOOTH_A2DP && volumeType == STREAM_MUSIC &&
