6fbd533482f198527aecbfd9409d16e23e1aee00
Jvgang <yujugang2@h-partners.com>
2025-04-26 11:53:39 +0000

    Only Built-in Mic Can Use 4 Channel, Update Later by Using XML to Describe For PC
    Signed-off-by: Jvgang <yujugang2@h-partners.com>

--- frameworks/native/hdiadapter_new/util/ring_buffer_handler.cpp
+++ frameworks/native/hdiadapter_new/util/ring_buffer_handler.cpp
@@ -43,8 +43,8 @@ void RingBufferHandler::Init(const uint32_t sampleRate, const uint32_t channelCo
     std::lock_guard<std::mutex> lock(mutex_);
     perFrameLength_ = ((sampleRate * onceFrameNum) / PER_FRAME_LENGTH_RATE) * channelCount * formatBytes;
     maxBufferSize_ = perFrameLength_ * maxFrameNum;
-    CHECK_AND_RETURN_LOG(maxBufferSize_ > 0 && maxBufferSize_ <= MAX_BUFFER_SIZE, "invalid param, maxBufferSize: %{public}u",
-        maxBufferSize_);
+    CHECK_AND_RETURN_LOG(maxBufferSize_ > 0 && maxBufferSize_ <= MAX_BUFFER_SIZE,
+        "invalid param, maxBufferSize: %{public}u", maxBufferSize_);
     maxFrameNum_ = maxFrameNum;
 
     buffer_ = new uint8_t[maxBufferSize_];
