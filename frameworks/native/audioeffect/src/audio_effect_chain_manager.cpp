6400682c44c21b3af09db68c843439e4fa031128
lizihao <lizihao56@h-partners.com>
2025-04-29 20:42:40 +0800

    修复PC平台设备切换无声问题
    Signed-off-by: lizihao <lizihao56@h-partners.com>

--- frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
+++ frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
@@ -1595,7 +1595,6 @@ void AudioEffectChainManager::WaitAndReleaseEffectChain(const std::string &scene
 
     if (sceneTypeToEffectChainCountMap_.count(sceneTypeAndDeviceKey) &&
         sceneTypeToEffectChainCountMap_[sceneTypeAndDeviceKey] == 0) {
-        sceneTypeToSpecialEffectSet_.erase(sceneType);
         sceneTypeToEffectChainCountMap_.erase(sceneTypeAndDeviceKey);
         if (ret == SUCCESS && defaultEffectChainCount_ == 0) {
             sceneTypeToEffectChainMap_.erase(defaultSceneTypeAndDeviceKey);
@@ -1630,6 +1629,7 @@ int32_t AudioEffectChainManager::ReleaseAudioEffectChainDynamicInner(const std::
     }
 
     sceneTypeToEffectChainCountMap_[sceneTypeAndDeviceKey] = 0;
+    sceneTypeToSpecialEffectSet_.erase(sceneType);
     int32_t ret = CheckAndReleaseCommonEffectChain(sceneType);
     std::thread([this, sceneType, sceneTypeAndDeviceKey, defaultSceneTypeAndDeviceKey, ret]() {
         WaitAndReleaseEffectChain(sceneType, sceneTypeAndDeviceKey, defaultSceneTypeAndDeviceKey, ret);
