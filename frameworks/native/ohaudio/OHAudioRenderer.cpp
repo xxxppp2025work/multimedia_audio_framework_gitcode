ce114f1639a3895504d214b317429e3ada7ee090
heyabinghyb <heyabing@h-partners.com>
2025-05-06 01:49:32 +0000

    检视意见修改
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- frameworks/native/ohaudio/OHAudioRenderer.cpp
+++ frameworks/native/ohaudio/OHAudioRenderer.cpp
@@ -457,8 +457,8 @@ bool OHAudioRenderer::Flush()
 bool OHAudioRenderer::Release()
 {
     std::unique_lock<std::mutex> lock(mtx_);
-    onWriteDataCv_.wait_for(lock, std::chrono::seconds(1), [this] { return isOnWriteData_;});
     isReleased_ = true;
+    onWriteDataCv_.wait_for(lock, std::chrono::seconds(1), [this] { return !isOnWriteData_;});
     lock.unlock();
     if (audioRenderer_ == nullptr) {
         AUDIO_ERR_LOG("renderer client is nullptr");
@@ -766,6 +766,7 @@ void OHAudioRendererModeCallback::OnWriteData(size_t length)
     std::unique_lock<std::mutex> lock(audioRenderer->mtx_);
     CHECK_AND_RETURN_LOG(!audioRenderer->isReleased_, "audioRenderer is released");
     audioRenderer->isOnWriteData_ = false;
+    lock.unlock();
     audioRenderer->GetBufferDesc(bufDesc);
     if (encodingType_ == ENCODING_AUDIOVIVID && writeDataWithMetadataCallback_ != nullptr) {
         writeDataWithMetadataCallback_(ohAudioRenderer_, metadataUserData_, (void*)bufDesc.buffer, bufDesc.bufLength,
