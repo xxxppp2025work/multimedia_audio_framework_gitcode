a1efc7e2dacaeb66ceb4ae89e86683e59df89667
yupanwen <yupanwen@huawei.com>
2025-05-16 14:26:36 +0800

    remove lock for trigger fetch device
    Signed-off-by: 136******92 <yupanwen@huawei.com>
    Change-Id: I3268b9087036902ab3a7a616ed5583c4184f2745

--- services/audio_policy/server/src/audio_policy_server.cpp
+++ services/audio_policy/server/src/audio_policy_server.cpp
@@ -3392,7 +3392,7 @@ int32_t AudioPolicyServer::RegisterAudioZoneClient(const sptr<IRemoteObject> &ob
 {
     CHECK_AND_RETURN_RET_LOG(object != nullptr, ERR_INVALID_PARAM,
         "RegisterAudioZoneClient listener object is nullptr");
-    
+
     sptr<IStandardAudioZoneClient> client = iface_cast<IStandardAudioZoneClient>(object);
     CHECK_AND_RETURN_RET_LOG(client != nullptr, ERR_INVALID_PARAM,
         "RegisterAudioZoneClient listener obj cast failed");
@@ -3643,8 +3643,8 @@ int32_t AudioPolicyServer::TriggerFetchDevice(AudioStreamDeviceChangeReasonExt r
     if (callerUid != UID_AUDIO) {
         return ERROR;
     }
-    CHECK_AND_RETURN_RET_LOG(eventEntry_ != nullptr, ERR_NULL_POINTER, "eventEntry_ is nullptr");
-    return eventEntry_->TriggerFetchDevice(reason);
+    CHECK_AND_RETURN_RET_LOG(coreService_ != nullptr, ERR_NULL_POINTER, "coreService_ is nullptr");
+    return coreService_->TriggerFetchDevice(reason);
 }
 
 int32_t AudioPolicyServer::SetPreferredDevice(const PreferredType preferredType,
