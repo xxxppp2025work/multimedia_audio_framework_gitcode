68e2522e39d8362f2a47049be6cbefb637f334b1
jiao <jiaoruiting@huawei.com>
2025-05-21 03:33:13 +0000

    update services/audio_policy/server/src/service/service/audio_device_common.cpp.
    Signed-off-by: jiao <jiaoruiting@huawei.com>

--- services/audio_policy/server/src/service/service/audio_device_common.cpp
+++ services/audio_policy/server/src/service/service/audio_device_common.cpp
@@ -1164,7 +1164,7 @@ bool AudioDeviceCommon::SelectRingerOrAlarmDevices(const vector<std::shared_ptr<
 int32_t AudioDeviceCommon::HandleDeviceChangeForFetchInputDevice(std::shared_ptr<AudioDeviceDescriptor> &desc,
     std::shared_ptr<AudioCapturerChangeInfo> &capturerChangeInfo)
 {
-    if (desc != nullptr && desc->deviceType_ == DEVICE_TYPE_NONE ||
+    if (desc != nullptr && (desc->deviceType_ == DEVICE_TYPE_NONE ||
         (IsSameDevice(desc, capturerChangeInfo->inputDeviceInfo) && desc->connectState_ != DEACTIVE_CONNECTED)) {
         AUDIO_WARNING_LOG("stream %{public}d device not change, no need move device", capturerChangeInfo->sessionId);
         std::shared_ptr<AudioDeviceDescriptor> preferredDesc =
@@ -1350,7 +1350,7 @@ void AudioDeviceCommon::FetchInputDeviceWhenNoRunningStream()
         desc = audioRouterCenter_.FetchInputDevice(SOURCE_TYPE_MIC, -1);
     }
 
-    if (desc != nullptr && desc->deviceType_ == DEVICE_TYPE_NONE || IsSameDevice(desc, tempDesc)) {
+    if (desc != nullptr && (desc->deviceType_ == DEVICE_TYPE_NONE || IsSameDevice(desc, tempDesc))) {
         AUDIO_DEBUG_LOG("input device is not change");
         return;
     }
