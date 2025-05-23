6986e202cedb392d1095bc12ea2b6d086374b1c6
sunruiyang <sunruiyang5@h-partners.com>
2025-05-23 17:41:23 +0800

    add2
    Signed-off-by: sunruiyang <sunruiyang5@h-partners.com>

--- services/audio_policy/server/src/service/interrupt/audio_interrupt_service.cpp
+++ services/audio_policy/server/src/service/interrupt/audio_interrupt_service.cpp
@@ -1737,13 +1737,12 @@ void AudioInterruptService::DeactivateAudioInterruptInternal(const int32_t zoneI
         AUDIO_DEBUG_LOG("stream (streamId %{public}u) is not active now", audioInterrupt.streamId);
         return;
     }
-
+    audioDeviceManager_.UpdateDefaultOutputDeviceWhenStopping(audioInterrupt.streamId);
     if (itZone->second->focusStrategy == AudioZoneFocusStrategy::DISTRIBUTED_FOCUS_STRATEGY) {
         AUDIO_INFO_LOG("zone: %{public}d distributed focus strategy not resume when deactivate interrupt",
             itZone->first);
         return;
     }
-    audioDeviceManager_.UpdateDefaultOutputDeviceWhenStopping(audioInterrupt.streamId);
     // resume if other session was forced paused or ducked
     ResumeAudioFocusList(zoneId, isSessionTimeout);
 
