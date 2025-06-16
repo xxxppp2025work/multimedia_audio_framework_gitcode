667f731d8e29601c4c2d10943aa1253424f7ba0e
magekkkk <lixin113@huawei.com>
2025-06-16 13:00:57 +0000

    move device listener files
    Signed-off-by: magekkkk <lixin113@huawei.com>
    Change-Id: I8958aaea5b21802837b9ef380d40fbcdccc17019

--- services/audio_policy/server/domain/device/listener/hdiservice/primary/audio_pnp_server.cpp
+++ services/audio_policy/server/domain/device/listener/hdiservice/primary/audio_pnp_server.cpp
@@ -21,10 +21,11 @@
 #include <poll.h>
 #include "securec.h"
 #include "osal_time.h"
+
 #include "audio_utils.h"
 #include "audio_errors.h"
-#include "audio_input_thread.h"
 #include "audio_policy_log.h"
+#include "audio_input_thread.h"
 #include "audio_socket_thread.h"
 
 using namespace std;
