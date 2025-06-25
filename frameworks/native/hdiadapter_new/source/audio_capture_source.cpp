643e146839c72f9d9e221b401bdbd1b91aed6e83
蒋志庆 <jiangzhiqing4@h-partners.com>
2025-06-25 20:45:18 +0800

    高保真bugfix  HDIAudioInputType，纯净人声模式下发时机
    Signed-off-by: 蒋志庆 <jiangzhiqing4@h-partners.com>

--- frameworks/native/hdiadapter_new/source/audio_capture_source.cpp
+++ frameworks/native/hdiadapter_new/source/audio_capture_source.cpp
@@ -650,7 +650,6 @@ enum AudioInputType AudioCaptureSource::ConvertToHDIAudioInputType(int32_t sourc
             break;
         case SOURCE_TYPE_LIVE:
             hdiAudioInputType = AUDIO_INPUT_LIVE_TYPE;
-            checkAcousticEchoCancelerSupported(hdiAudioInputType);
             break;
         default:
             hdiAudioInputType = AUDIO_INPUT_MIC_TYPE;
@@ -659,8 +658,9 @@ enum AudioInputType AudioCaptureSource::ConvertToHDIAudioInputType(int32_t sourc
     return hdiAudioInputType;
 }
 
-void AudioCaptureSource::checkAcousticEchoCancelerSupported(int32_t &hdiAudioInputType)
+void AudioCaptureSource::checkAcousticEchoCancelerSupported(int32_t sourceType, int32_t &hdiAudioInputType)
 {
+    CHECK_AND_RETURN(sourceType == SOURCE_TYPE_LIVE);
     HdiAdapterManager &manager = HdiAdapterManager::GetInstance();
     std::shared_ptr<IDeviceManager> deviceManager = manager.GetDeviceManager(HDI_DEVICE_MANAGER_TYPE_LOCAL);
     CHECK_AND_RETURN_LOG(deviceManager != nullptr, "local device manager is nullptr");
@@ -817,7 +817,7 @@ void AudioCaptureSource::InitAudioSampleAttr(struct AudioSampleAttributes &param
         param.startThreshold = DEEP_BUFFER_CAPTURE_PERIOD_SIZE / (param.frameSize);
     }
     param.sourceType = static_cast<int32_t>(ConvertToHDIAudioInputType(attr_.sourceType));
-
+    checkAcousticEchoCancelerSupported(attr_.sourceType, param.sourceType);
     if ((attr_.hasEcConfig || attr_.sourceType == SOURCE_TYPE_EC) && attr_.channelEc != 0) {
         param.ecSampleAttributes.ecInterleaved = true;
         param.ecSampleAttributes.ecFormat = ConvertToHdiFormat(attr_.formatEc);
@@ -925,6 +925,7 @@ int32_t AudioCaptureSource::DoSetInputRoute(DeviceType inputDevice)
     CHECK_AND_RETURN_RET(deviceManager != nullptr, ERR_INVALID_HANDLE);
     int32_t streamId = static_cast<int32_t>(GetUniqueIdBySourceType());
     int32_t inputType = static_cast<int32_t>(ConvertToHDIAudioInputType(attr_.sourceType));
+    checkAcousticEchoCancelerSupported(attr_.sourceType, inputType);
     AUDIO_INFO_LOG("adapterName: %{public}s, inputDevice: %{public}d, streamId: %{public}d, inputType: %{public}d",
         attr_.adapterName.c_str(), inputDevice, streamId, inputType);
     int32_t ret = deviceManager->SetInputRoute(adapterNameCase_, inputDevice, streamId, inputType);
