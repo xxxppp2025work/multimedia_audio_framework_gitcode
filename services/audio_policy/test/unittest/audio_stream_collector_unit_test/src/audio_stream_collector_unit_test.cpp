9923b82fe6ae9751798ea3e91c48c45b3fb2d804
Vacord <duanmingxu@huawei.com>
2025-07-07 01:29:34 +0000

    update audio_stream_collector_unit_test/src/audio_stream_collector_unit_test.cpp.
    Signed-off-by: Vacord <duanmingxu@huawei.com>

--- services/audio_policy/test/unittest/audio_stream_collector_unit_test/src/audio_stream_collector_unit_test.cpp
+++ services/audio_policy/test/unittest/audio_stream_collector_unit_test/src/audio_stream_collector_unit_test.cpp
@@ -1951,52 +1951,52 @@ HWTEST_F(AudioStreamCollectorUnitTest, HandleStartStreamMuteState_002, TestSize.
 }
 
 /**
-* @tc.name  : Test GetRunningRenderInfos.
-* @tc.number: GetRunningRenderInfos_001
-* @tc.desc  : Test GetRunningRenderInfos.
+* @tc.name  : Test GetRunningRendererInfos.
+* @tc.number: GetRunningRendererInfos_001
+* @tc.desc  : Test GetRunningRendererInfos.
 */
-HWTEST_F(AudioStreamCollectorUnitTest, GetRunningRenderInfos_001, TestSize.Level1)
+HWTEST_F(AudioStreamCollectorUnitTest, GetRunningRendererInfos_001, TestSize.Level1)
 {
     AudioStreamCollector audioStreamCollector_;
     std::vector<std::shared_ptr<AudioRendererChangeInfo>> infos;
-    int32_t result = audioStreamCollector_.GetRunningRenderInfos(infos);
+    int32_t result = audioStreamCollector_.GetRunningRendererInfos(infos);
     EXPECT_EQ(result, SUCCESS);
     EXPECT_TRUE(infos.empty());
 }
 
 /**
-* @tc.name  : Test GetRunningRenderInfos.
-* @tc.number: GetRunningRenderInfos_002
-* @tc.desc  : Test GetRunningRenderInfos.
+* @tc.name  : Test GetRunningRendererInfos.
+* @tc.number: GetRunningRendererInfos_002
+* @tc.desc  : Test GetRunningRendererInfos.
 */
-HWTEST_F(AudioStreamCollectorUnitTest, GetRunningRenderInfos_002, TestSize.Level1)
+HWTEST_F(AudioStreamCollectorUnitTest, GetRunningRendererInfos_002, TestSize.Level1)
 {
     AudioStreamCollector audioStreamCollector_;
-    audioStreamCollector_audioRenderChangeInfos_.push_back(std::shared_ptr<AudioRendererChangeInfo>());
-    audioStreamCollector_audioRenderChangeInfos_.back()->rendererState = RENDERER_RUNNING;
+    audioStreamCollector_audioRendererChangeInfos_.push_back(std::shared_ptr<AudioRendererChangeInfo>());
+    audioStreamCollector_audioRendererChangeInfos_.back()->rendererState = RENDERER_RUNNING;
     std::vector<std::shared_ptr<AudioRendererChangeInfo>> infos;
-    int32_t result = audioStreamCollector_.GetRunningRenderInfos(infos);
+    int32_t result = audioStreamCollector_.GetRunningRendererInfos(infos);
     EXPECT_EQ(result, SUCCESS);
     EXPECT_EQ(infos.size(), 1);
     EXPECT_TRUE(infos[0]->rendererState,RENDERER_RUNNING);
 }
 
 /**
-* @tc.name  : Test GetRunningRenderInfos.
-* @tc.number: GetRunningRenderInfos_003
-* @tc.desc  : Test GetRunningRenderInfos.
+* @tc.name  : Test GetRunningRendererInfos.
+* @tc.number: GetRunningRendererInfos_003
+* @tc.desc  : Test GetRunningRendererInfos.
 */
-HWTEST_F(AudioStreamCollectorUnitTest, GetRunningRenderInfos_003, TestSize.Level1)
+HWTEST_F(AudioStreamCollectorUnitTest, GetRunningRendererInfos_003, TestSize.Level1)
 {
     AudioStreamCollector audioStreamCollector_;
     auto info1 = std::shared_ptr<AudioRendererChangeInfo>();
     info1->rendererState = RENDERER_RUNNING;
-    audioStreamCollector_audioRenderChangeInfos_.push_back(info1);
+    audioStreamCollector_audioRendererChangeInfos_.push_back(info1);
     auto info2 = std::shared_ptr<AudioRendererChangeInfo>();
     info2->rendererState = RENDERER_STOPPED;
-    audioStreamCollector_audioRenderChangeInfos_.push_back(info2);
+    audioStreamCollector_audioRendererChangeInfos_.push_back(info2);
     std::vector<std::shared_ptr<AudioRendererChangeInfo>> infos;
-    int32_t result = audioStreamCollector_.GetRunningRenderInfos(infos);
+    int32_t result = audioStreamCollector_.GetRunningRendererInfos(infos);
     EXPECT_EQ(result, SUCCESS);
     EXPECT_EQ(infos.size(), 1);
     EXPECT_TRUE(infos[0]->rendererState,RENDERER_RUNNING);
