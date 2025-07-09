932b2cc55a70cfbe50ca88a705c72440db5205d2
vacord <duanmingxu@huawei.com>
2025-07-09 16:23:59 +0800

    音频用例code modify
    Signed-off-by: vacord <duanmingxu@huawei.com>

--- services/audio_policy/test/unittest/audio_interrupt_service_test/src/audio_policy_server_unit_test_second.cpp
+++ services/audio_policy/test/unittest/audio_interrupt_service_test/src/audio_policy_server_unit_test_second.cpp
@@ -380,6 +380,7 @@ HWTEST(AudioPolicyUnitTestSecond, AudioPolicyServer_216, TestSize.Level1)
     EXPECT_EQ(ret, ERROR);
 }
 
+#ifdef FEATURE_MULTIMODALINPUT_INPUT
 /**
 * @tc.name  : IsContinueAddVolTest_001
 * @tc.number: IsContinueAddVolTest_001
@@ -500,5 +501,6 @@ HWTEST(AudioPolicyUnitTest, ProcessVolumeKeyEvents_001, TestSize.Level1)
     EXPECT_NE(ptrAudioPolicyServer, nullptr);
     ptrAudioPolicyServer->ProcessVolumeKeyEvents(keyType);
 }
+#endif
 } // AudioStandard
 } // OHOS
