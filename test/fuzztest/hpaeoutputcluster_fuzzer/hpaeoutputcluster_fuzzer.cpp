f20fb5b97b453a8828b13ad89722c428d877ecd5
Admin <12234755+xm_13_0@user.noreply.gitee.com>
2025-07-17 11:54:46 +0800

    fuzz
    Signed-off-by: Admin <12234755+xm_13_0@user.noreply.gitee.com>

--- test/fuzztest/hpaeoutputcluster_fuzzer/hpaeoutputcluster_fuzzer.cpp
+++ test/fuzztest/hpaeoutputcluster_fuzzer/hpaeoutputcluster_fuzzer.cpp
@@ -36,7 +36,7 @@ static const uint8_t *RAW_DATA = nullptr;
 static size_t g_dataSize = 0;
 static size_t g_pos;
 const size_t THRESHOLD = 10;
-typedef void (*TestPtr)(const uint8_t *,size_t)
+typedef void (*TestPtr)(const uint8_t *,size_t);
 
 template<class T>
 T GetData()
