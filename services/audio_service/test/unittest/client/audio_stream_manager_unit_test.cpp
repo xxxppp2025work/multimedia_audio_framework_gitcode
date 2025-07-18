82e7e984ee71afd7f9c52105b089a6d0ee58fb10
杨江诚 <yangjiangcheng1@huawei.com>
2025-07-18 04:40:33 +0000

    update services/audio_service/test/unittest/client/audio_stream_manager_unit_test.cpp.
    Signed-off-by: 杨江诚 <yangjiangcheng1@huawei.com>

--- services/audio_service/test/unittest/client/audio_stream_manager_unit_test.cpp
+++ services/audio_service/test/unittest/client/audio_stream_manager_unit_test.cpp
@@ -58,6 +58,28 @@ HWTEST(AudioStreamManagerUnitTest, IsStreamActive_001, TestSize.Level1)
     EXPECT_EQ(result, false);
 }
 /**
+ * @tc.name  : Test IsStreamActiveByStreamUsage API
+ * @tc.number: IsStreamActiveByStreamUsage_001
+ * @tc.desc  : Test IsStreamActiveByStreamUsage interface.
+ */
+HWTEST(AudioStreamManagerUnitTest, IsStreamActiveByStreamUsage_001, TestSize.Level1)
+{
+    StreamUsage ILLEGAL_STREAM_USAGE = static_cast<StreamUsage>(static_cast<int32_t>(STREAM_USAGE_MAX)+999);
+
+    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 start");
+    bool result = AudioStreamManager::GetInstance()->IsStreamActiveByStreamUsage(STREAM_USAGE_MUSIC);
+    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 result1:%{public}d", result);
+    EXPECT_EQ(result, false);
+
+    result = AudioStreamManager::GetInstance()->IsStreamActiveByStreamUsage(STREAM_USAGE_ULTRASONIC);
+    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 result2:%{public}d", result);
+    EXPECT_EQ(result, false);
+
+    result = AudioStreamManager::GetInstance()->IsStreamActiveByStreamUsage(ILLEGAL_STREAM_USAGE);
+    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 result2:%{public}d", result);
+    EXPECT_EQ(result, false);
+}
+/**
  * @tc.name  : Test GetHardeareOutputSamplingRate API
  * @tc.type  : FUNC
  * @tc.number: GetHardeareOutputSamplingRate_001
@@ -74,9 +96,6 @@ HWTEST(AudioStreamManagerUnitTest, GetHardwareOutputSamplingRate_001, TestSize.L
 /**
  * @tc.name  : Test IsStreamActiveByStreamUsage API
  * @tc.type  : FUNC
-
-/**
- * @tc.name  : Test IsStreamActiveByStreamUsage API
  * @tc.number: IsStreamActiveByStreamUsage_001
  * @tc.desc  : Test IsStreamActiveByStreamUsage interface.
  */
@@ -157,20 +176,6 @@ HWTEST(AudioStreamManagerUnitTest, IsAudioLoopbackSupported_001, TestSize.Level1
     int32_t result = AudioStreamManager::GetInstance()->IsAudioLoopbackSupported(mode);
 
     EXPECT_EQ(result, 1);
-    StreamUsage ILLEGAL_STREAM_USAGE = static_cast<StreamUsage>(static_cast<int32_t>(STREAM_USAGE_MAX)+999);
-
-    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 start");
-    bool result = AudioStreamManager::GetInstance()->IsStreamActiveByStreamUsage(STREAM_USAGE_MUSIC);
-    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 result1:%{public}d", result);
-    EXPECT_EQ(result, false);
-
-    result = AudioStreamManager::GetInstance()->IsStreamActiveByStreamUsage(STREAM_USAGE_ULTRASONIC);
-    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 result2:%{public}d", result);
-    EXPECT_EQ(result, false);
-
-    result = AudioStreamManager::GetInstance()->IsStreamActiveByStreamUsage(ILLEGAL_STREAM_USAGE);
-    AUDIO_INFO_LOG("AudioSystemManagerUnitTest IsStreamActiveByStreamUsage_001 result2:%{public}d", result);
-    EXPECT_EQ(result, false);
 }
 } // namespace AudioStandard
 } // namespace OHOS
