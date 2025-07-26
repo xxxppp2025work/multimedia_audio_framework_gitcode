19d8e110ca7c329b8a2dad2fed42ca37afece3a3
杨临风 <yanglinfeng5@huawei.com>
2025-07-26 18:15:11 +0800

    cleancode modify
    Signed-off-by: 杨临风 <yanglinfeng5@huawei.com>

--- frameworks/native/pulseaudio/modules/hdi/hdi_adapter_intf/source/source_intf.c
+++ frameworks/native/pulseaudio/modules/hdi/hdi_adapter_intf/source/source_intf.c
@@ -62,6 +62,9 @@ struct SourceAdapter *GetSourceAdapter(const char *deviceClass, const int32_t so
     int32_t ret = InitSourceAdapter(adapter, deviceClass, sourceType, info);
     if (ret != 0) {
         AUDIO_ERR_LOG("not support, deviceClass: %{public}s, info: %{public}s", deviceClass, info);
+        if (adapter->deviceClass != NULL) {
+            free((char *)(adapter->deviceClass));
+        }
         free(adapter);
         return NULL;
     }
