ec2c07ab206c67b6d762ce4ec603425171dfa395
heyabinghyb <heyabing@h-partners.com>
2025-07-26 11:23:19 +0000

    update frameworks/native/ohaudio/OHAudioRoutingManager.cpp.
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- frameworks/native/ohaudio/OHAudioRoutingManager.cpp
+++ frameworks/native/ohaudio/OHAudioRoutingManager.cpp
@@ -75,7 +75,7 @@ OH_AudioCommon_Result OH_AudioManager_GetAudioRoutingManager(OH_AudioRoutingMana
     OHAudioRoutingManager* ohAudioRoutingManager = OHAudioRoutingManager::GetInstance();
     if (audioRoutingManager == nullptr) {
         AUDIO_ERR_LOG("audioRoutingManager is nullptr!");
-        return AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM;
+        return AUDIOCOMMON_RESULT_SUCCESS;
     }
     *audioRoutingManager = (OH_AudioRoutingManager*)ohAudioRoutingManager;
     return AUDIOCOMMON_RESULT_SUCCESS;
