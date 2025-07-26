92ae45a4d7a104ba9795ac4d04e5b4f15e322228
weiyuantian <weiyuantian1@huawei.com>
2025-07-26 07:07:28 +0000

    update frameworks/native/audioinnercall/test/unittest/audio_inner_call_unit_test.cpp.
    Signed-off-by: weiyuantian <weiyuantian1@huawei.com>

--- frameworks/native/audioinnercall/test/unittest/audio_inner_call_unit_test.cpp
+++ frameworks/native/audioinnercall/test/unittest/audio_inner_call_unit_test.cpp
@@ -27,7 +27,9 @@ public:
     static void SetUpTestCase(){};
     static void TearDownTestCase(){};
     virtual void SetUp(){};
-    virtual void TearDown() {}
+    virtual void TearDown() {
+        AudioInnerCall::ReleaseInstance();
+    }
 };
 
 /**
