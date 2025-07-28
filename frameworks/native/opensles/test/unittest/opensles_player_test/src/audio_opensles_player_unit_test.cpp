ba4cad512bea6883af5b0aaf7c4f007ae8aa36aa
weiyuantian <weiyuantian1@huawei.com>
2025-07-28 06:21:22 +0000

    update opensles/test/unittest/opensles_player_test/src/audio_opensles_player_unit_test.cpp.
    Signed-off-by: weiyuantian <weiyuantian1@huawei.com>

--- frameworks/native/opensles/test/unittest/opensles_player_test/src/audio_opensles_player_unit_test.cpp
+++ frameworks/native/opensles/test/unittest/opensles_player_test/src/audio_opensles_player_unit_test.cpp
@@ -1389,9 +1389,9 @@ HWTEST(AudioOpenslesPlayerUnitTest, SetRecordState_001, TestSize.Level1)
 {
     SLRecordItf self = {};
     SLuint32 state = 1;
-    SLresult result = (*recordItf_)->SetRecordState(nullptr, &state);
+    SLresult result = (*recordItf_)->SetRecordState(nullptr, state);
     EXPECT_EQ(result, SL_RESULT_PARAMETER_INVALID);
-    result = (*recordItf_)->SetRecordState(self, &state);
+    result = (*recordItf_)->SetRecordState(self, state);
     EXPECT_EQ(result, SL_RESULT_SUCCESS);
 }
 
@@ -1399,9 +1399,9 @@ HWTEST(AudioOpenslesPlayerUnitTest, GetRecordState_001, TestSize.Level1)
 {
     SLRecordItf self = {};
     SLuint32 state = 1;
-    SLresult result = (*recordItf_)->GetRecordState(nullptr, state);
+    SLresult result = (*recordItf_)->GetRecordState(nullptr, &state);
     EXPECT_EQ(result, SL_RESULT_PARAMETER_INVALID);
-    result = (*recordItf_)->GetRecordState(self, state);
+    result = (*recordItf_)->GetRecordState(self, &state);
     EXPECT_EQ(result, SL_RESULT_SUCCESS);
 }
 } // namespace AudioStandard
