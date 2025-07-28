edab25a06ac00eb1ec971979d4dd49b209e5df12
weiyuantian <weiyuantian1@huawei.com>
2025-07-28 03:38:21 +0000

    update frameworks/native/audioqosmanager/test/unittest/audio_qosmanager_unit_test.cpp.
    Signed-off-by: weiyuantian <weiyuantian1@huawei.com>

--- frameworks/native/audioqosmanager/test/unittest/audio_qosmanager_unit_test.cpp
+++ frameworks/native/audioqosmanager/test/unittest/audio_qosmanager_unit_test.cpp
@@ -24,7 +24,7 @@ public:
     static void SetUpTestCase(){};
     static void TearDownTestCase(){};
     virtual void SetUp(){};
-    virtual void TearDown() {}
+    virtual void TearDown(){};
 };
 
 /**
