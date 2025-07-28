df81cd7069d898ec202b63210e1aee5b18d9d8b9
weiyuantian <weiyuantian1@huawei.com>
2025-07-28 07:21:25 +0000

    update frameworks/native/audiodefinitions/test/unittest/audio_stream_descriptor_unit_test.cpp.
    Signed-off-by: weiyuantian <weiyuantian1@huawei.com>

--- frameworks/native/audiodefinitions/test/unittest/audio_stream_descriptor_unit_test.cpp
+++ frameworks/native/audiodefinitions/test/unittest/audio_stream_descriptor_unit_test.cpp
@@ -45,11 +45,8 @@ HWTEST_F(AudioStreamDescriptorUnitTest, WriteDeviceDescVectorToParcel_001, TestS
 {
     AudioStreamDescriptor audioStreamDescriptor;
     Parcel parcel;
-    std::vector<std::shared_ptr<AudioDeviceDescriptor>> descs;
-    descs.reserve(MAX_STREAM_DESCRIPTORS_SIZE);
-    for (int i = 0; i < MAX_STREAM_DESCRIPTORS_SIZE; i++) {
-        descs.push_back(std::make_shared<AudioDeviceDescriptor>(DEVICE_TYPE_SPEAKER, OUTPUT_DEVICE));
-    }
+    std::vector<std::shared_ptr<AudioDeviceDescriptor>> descs(MAX_STREAM_DESCRIPTORS_SIZE,
+        std::make_shared<AudioDeviceDescriptor>(DEVICE_TYPE_SPEAKER, OUTPUT_DEVICE));
     EXPECT_TRUE(audioStreamDescriptor.WriteDeviceDescVectorToParcel(parcel, descs));
 }
 
