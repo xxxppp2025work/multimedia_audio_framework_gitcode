99cd8efa04411900b2544c145b2ca92934b38e93
杨江诚 <yangjiangcheng1@huawei.com>
2025-07-28 12:34:50 +0000

    update services/audio_service/test/unittest/client/fast_audio_stream_unit_test.cpp.
    Signed-off-by: 杨江诚 <yangjiangcheng1@huawei.com>

--- services/audio_service/test/unittest/client/fast_audio_stream_unit_test.cpp
+++ services/audio_service/test/unittest/client/fast_audio_stream_unit_test.cpp
@@ -1632,25 +1632,6 @@ HWTEST(FastSystemStreamUnitTest, CheckRestoreStatus_001, TestSize.Level1)
 }
 
 /**
- * @tc.name  : Test CheckRestoreStatus API
- * @tc.type  : FUNC
- * @tc.number: CheckRestoreStatus_001
- * @tc.desc  : Test CheckRestoreStatus interface. - return RESTORE_TO_NORMAL when spkProcClientCb_ is null
- */
-HWTEST(FastSystemStreamUnitTest, CheckRestoreStatus_001, TestSize.Level1)
-{
-    int32_t appUid = static_cast<int32_t>(getuid());
-    std::shared_ptr<FastAudioStream> fastAudioStream =
-        std::make_shared<FastAudioStream>(STREAM_MUSIC, AUDIO_MODE_PLAYBACK, appUid);
-    EXPECT_NE(fastAudioStream, nullptr);
-
-    fastAudioStream->spkProcClientCb_ = nullptr;
-    fastAudioStream->micProcClientCb_ = nullptr;
-    RestoreStatus status = fastAudioStream->CheckRestoreStatus();
-    EXPECT_EQ(status, NEED_RESTORE_TO_NORMAL);
-}
-
-/**
  * @tc.name  : Test SetCallbacksWhenRestore API
  * @tc.type  : FUNC
  * @tc.number: SetCallbacksWhenRestore_001
