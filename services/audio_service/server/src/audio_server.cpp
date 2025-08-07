93f99aa499d9e5737061829b6121effc968300d1
马广生 <maguangsheng1@h-partners.com>
2025-08-07 08:42:38 +0000

    print compatible with arm64
    Signed-off-by: 马广生 <maguangsheng1@h-partners.com>

--- services/audio_service/server/src/audio_server.cpp
+++ services/audio_service/server/src/audio_server.cpp
@@ -1310,7 +1310,7 @@ int32_t AudioServer::SetIORoutes(std::vector<std::pair<DeviceType, DeviceFlag>> 
     for (auto activeDevice : activeDevices) {
         deviceTypes.push_back(activeDevice.first);
     }
-    AUDIO_INFO_LOG("SetIORoutes 1st deviceType: %{public}d, deviceSize : %{public}d, flag: %{public}d",
+    AUDIO_INFO_LOG("SetIORoutes 1st deviceType: %{public}d, deviceSize : %{public}zu, flag: %{public}d",
         type, deviceTypes.size(), flag);
     int32_t ret = SetIORoutes(type, flag, deviceTypes, a2dpOffloadFlag, deviceName);
     return ret;
