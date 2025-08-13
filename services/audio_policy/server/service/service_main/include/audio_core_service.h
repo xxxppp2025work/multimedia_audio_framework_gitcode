bcf6bf644ff55a3543693e4d284be4f3ba1c1711
heyabinghyb <heyabing@h-partners.com>
2025-08-13 16:16:15 +0000

    update services/audio_policy/server/service/service_main/include/audio_core_service.h.
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- services/audio_policy/server/service/service_main/include/audio_core_service.h
+++ services/audio_policy/server/service/service_main/include/audio_core_service.h
@@ -482,7 +482,7 @@ private:
     // for collaboration
     void UpdateRouteForCollaboration(InternalDeviceType deviceType);
     int32_t CheckAndUpdateHearingAidCall(const DeviceType deviceType);
-    void CheckModuleForHearingAid();
+    void CheckModuleForHearingAid(uint32_t paIndex);
     void CheckCloseHearingAidCall(const bool isModemCallRunning, const DeviceType type);
     void CheckOpenHearingAidCall(const bool isModemCallRunning, const DeviceType type);
 
