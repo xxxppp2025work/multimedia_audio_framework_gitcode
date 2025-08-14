4147af5b36505162c0396bb60c8d1e877fc71f8b
Gisaac <gongjie13@huawei.com>
2025-08-14 01:37:35 +0000

    update services/audio_policy/server/service/service_main/include/audio_core_service.h.
    Signed-off-by: Gisaac <gongjie13@huawei.com>

--- services/audio_policy/server/service/service_main/include/audio_core_service.h
+++ services/audio_policy/server/service/service_main/include/audio_core_service.h
@@ -482,7 +482,7 @@ private:
     // for collaboration
     void UpdateRouteForCollaboration(InternalDeviceType deviceType);
     int32_t CheckAndUpdateHearingAidCall(const DeviceType deviceType);
-    void CheckModuleForHearingAid(const DeviceType deviceType);
+    void CheckModuleForHearingAid();
     void CheckCloseHearingAidCall(const bool isModemCallRunning, const DeviceType type);
     void CheckOpenHearingAidCall(const bool isModemCallRunning, const DeviceType type);
 
