79d16b706e02460ec8b975f2deebdc89361b1ba5
陈耀文 <chenyaowen6@huawei.com>
2025-08-18 16:34:37 +0800

    支持清除通话强选需求开发
    Signed-off-by: 陈耀文 <chenyaowen6@huawei.com>

--- services/audio_policy/server/service/service_main/src/audio_core_service_entry.cpp
+++ services/audio_policy/server/service/service_main/src/audio_core_service_entry.cpp
@@ -380,7 +380,7 @@ void AudioCoreService::EventEntry::OnReceiveBluetoothEvent(const std::string mac
 }
 
 int32_t AudioCoreService::EventEntry::SelectOutputDevice(sptr<AudioRendererFilter> audioRendererFilter,
-    std::vector<std::shared_ptr<AudioDeviceDescriptor>> selectedDesc)
+    std::vector<std::shared_ptr<AudioDeviceDescriptor>> selectedDesc, const int32_t audioDeviceSelectMode)
 {
     Trace trace("KeyAction AudioCoreService::SelectOutputDevice");
     if (!selectedDesc.empty() && selectedDesc[0] && coreService_ &&
@@ -388,7 +388,7 @@ int32_t AudioCoreService::EventEntry::SelectOutputDevice(sptr<AudioRendererFilte
         coreService_->NotifyDistributedOutputChange(selectedDesc[0]);
     }
     std::lock_guard<std::shared_mutex> lock(eventMutex_);
-    return coreService_->SelectOutputDevice(audioRendererFilter, selectedDesc);
+    return coreService_->SelectOutputDevice(audioRendererFilter, selectedDesc, audioDeviceSelectMode);
 }
 
 int32_t AudioCoreService::EventEntry::SelectInputDevice(sptr<AudioCapturerFilter> audioCapturerFilter,
