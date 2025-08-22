6c8cb4da21915f3b218940d4cbada6fb5d360168
shiju <shiju1@huawei.com>
2025-08-22 08:32:11 +0000

    update test/unittest/playback_capturer_manager_unit_test.cpp.
    Signed-off-by: shiju <shiju1@huawei.com>

--- frameworks/native/playbackcapturer/test/unittest/playback_capturer_manager_unit_test.cpp
+++ frameworks/native/playbackcapturer/test/unittest/playback_capturer_manager_unit_test.cpp
@@ -138,16 +138,16 @@ HWTEST(PlaybackPlaybackCapturerManagerUnitTest, CheckCaptureLimit_001, TestSize.
 HWTEST(PlaybackPlaybackCapturerManagerUnitTest, SetSupportStreamUsage_001, TestSize.Level1)
 {
     std::vector<int32_t> usage;
-    PlaybackCapturerManager playbackCapturerManager01;
+    PlaybackCapturerManager firstPlaybackCapturerManager;
     int32_t temp = STREAM_USAGE_MEDIA;
     usage.emplace_back(temp);
-    playbackCapturerManager01.SetSupportStreamUsage(usage);
-    EXPECT_TRUE(playbackCapturerManager01.IsStreamSupportInnerCapturer(STREAM_USAGE_MEDIA));
+    firstPlaybackCapturerManager.SetSupportStreamUsage(usage);
+    EXPECT_TRUE(firstPlaybackCapturerManager.IsStreamSupportInnerCapturer(STREAM_USAGE_MEDIA));
 
-    PlaybackCapturerManager playbackCapturerManager02;
+    PlaybackCapturerManager secondPlaybackCapturerManager;
     usage.pop_back();
-    playbackCapturerManager02.SetSupportStreamUsage(usage);
-    EXPECT_TRUE(playbackCapturerManager02.IsStreamSupportInnerCapturer(STREAM_USAGE_MEDIA));
+    secondPlaybackCapturerManager.SetSupportStreamUsage(usage);
+    EXPECT_TRUE(secondPlaybackCapturerManager.IsStreamSupportInnerCapturer(STREAM_USAGE_MEDIA));
 }
 
 /**
