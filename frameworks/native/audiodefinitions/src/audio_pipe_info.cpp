9fd41b0ea8443375fb15e601fa8ca4987e8d0dc8
CodeRepository1 <16179862+coderepository1@user.noreply.gitee.com>
2025-09-02 01:39:07 +0000

    update frameworks/native/audiodefinitions/src/audio_pipe_info.cpp. synchronize yellow code.
    Signed-off-by: CodeRepository1 <16179862+coderepository1@user.noreply.gitee.com>

--- frameworks/native/audiodefinitions/src/audio_pipe_info.cpp
+++ frameworks/native/audiodefinitions/src/audio_pipe_info.cpp
@@ -123,7 +123,7 @@ std::string AudioPipeInfo::ToString()
 {
     std::string out = "";
     AppendFormat(out, "id %u, adapter %s, name %s",
-        id_, adapterName_.c_str(), name_.c_str());
+        id_, adapterName_.c_str(), name_.c_str());//update
     return out;
 }
 
