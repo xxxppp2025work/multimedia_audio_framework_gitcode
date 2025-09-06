c6207f777c65b29e65428a60ed9e7269f6f6502f
sunshinglight <chenyugan1@huawei.com>
2025-09-06 12:22:30 +0000

    illegal streamStatus change
    Signed-off-by: sunshinglight <chenyugan1@huawei.com>
    Change-Id: I63b0ccf9c3d0c48de8b62ed3e885b58588a9986b

--- services/audio_service/server/src/audio_endpoint.cpp
+++ services/audio_service/server/src/audio_endpoint.cpp
@@ -1977,10 +1977,10 @@ void AudioEndpointInner::WriteToProcessBuffers(const BufferDesc &readBuf)
     std::lock_guard<std::mutex> lock(listLock_);
     for (size_t i = 0; i < processBufferList_.size(); i++) {
         CHECK_AND_CONTINUE_LOG(processBufferList_[i] != nullptr, "process buffer %{public}zu is null.", i);
-        if (processBufferList_[i]->GetStreamStatus() &&
-            processBufferList_[i]->GetStreamStatus()->load() != STREAM_RUNNING) {
+        CHECK_AND_CONTINUE_LOG(processList_[i] != nullptr, "process %{public}zu is null.", i);
+        if (processList_[i]->GetStreamInServerStatus() != STREAM_RUNNING) {
             AUDIO_WARNING_LOG("process buffer %{public}zu not running, stream status %{public}d.",
-                i, processBufferList_[i]->GetStreamStatus()->load());
+                i, processList_[i]->GetStreamInServerStatus());
             continue;
         }
 
