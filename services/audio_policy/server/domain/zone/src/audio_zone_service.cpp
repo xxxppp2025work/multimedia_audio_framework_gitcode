3a54fb5fdd086b50f353b76259190528867d014b
darkshadow <chenyan218@huawei.com>
2025-09-06 16:20:49 +0800

    修复音区和焦点死锁问题
    Signed-off-by: darkshadow <chenyan218@huawei.com>

--- services/audio_policy/server/domain/zone/src/audio_zone_service.cpp
+++ services/audio_policy/server/domain/zone/src/audio_zone_service.cpp
@@ -75,8 +75,8 @@ int32_t AudioZoneService::CreateAudioZone(const std::string &name, const AudioZo
         }
 
         CHECK_AND_RETURN_RET_LOG(interruptService_ != nullptr, ERROR, "interruptService_ is nullptr");
-        interruptService_->CreateAudioInterruptZone(zoneId, context.focusStrategy_);
     }
+    interruptService_->CreateAudioInterruptZone(zoneId, context.focusStrategy_);
     AUDIO_INFO_LOG("create zone id %{public}d, name %{public}s", zoneId, name.c_str());
     return zoneId;
 }
@@ -496,10 +496,12 @@ int32_t AudioZoneService::GetSystemVolumeLevel(int32_t zoneId, AudioVolumeType v
 
 AudioZoneFocusList AudioZoneService::GetAudioInterruptForZone(int32_t zoneId)
 {
-    std::lock_guard<std::mutex> lock(zoneMutex_);
-    AudioZoneFocusList interrupts;
-    CHECK_AND_RETURN_RET_LOG(CheckIsZoneValid(zoneId), interrupts, "zone id %{public}d is not valid", zoneId);
-    CHECK_AND_RETURN_RET_LOG(interruptService_ != nullptr, interrupts, "interruptService_ is nullptr");
+    {
+        std::lock_guard<std::mutex> lock(zoneMutex_);
+        AudioZoneFocusList interrupts;
+        CHECK_AND_RETURN_RET_LOG(CheckIsZoneValid(zoneId), interrupts, "zone id %{public}d is not valid", zoneId);
+        CHECK_AND_RETURN_RET_LOG(interruptService_ != nullptr, interrupts, "interruptService_ is nullptr");
+    }
 
     interruptService_->GetAudioFocusInfoList(zoneId, interrupts);
     return interrupts;
@@ -524,10 +526,12 @@ bool AudioZoneService::CheckZoneExist(int32_t zoneId)
 
 AudioZoneFocusList AudioZoneService::GetAudioInterruptForZone(int32_t zoneId, const std::string &deviceTag)
 {
-    std::lock_guard<std::mutex> lock(zoneMutex_);
-    AudioZoneFocusList interrupts;
-    CHECK_AND_RETURN_RET_LOG(CheckIsZoneValid(zoneId), interrupts, "zone id %{public}d is not valid", zoneId);
-    CHECK_AND_RETURN_RET_LOG(interruptService_ != nullptr, interrupts, "interruptService_ is nullptr");
+    {
+        std::lock_guard<std::mutex> lock(zoneMutex_);
+        AudioZoneFocusList interrupts;
+        CHECK_AND_RETURN_RET_LOG(CheckIsZoneValid(zoneId), interrupts, "zone id %{public}d is not valid", zoneId);
+        CHECK_AND_RETURN_RET_LOG(interruptService_ != nullptr, interrupts, "interruptService_ is nullptr");
+    }
 
     interruptService_->GetAudioFocusInfoList(zoneId, deviceTag, interrupts);
     return interrupts;
