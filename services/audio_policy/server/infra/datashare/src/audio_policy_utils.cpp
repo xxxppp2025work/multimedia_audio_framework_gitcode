fa7484fd9d3235c4c538763df7d286daad3b5ec3
yaojingyu2008 <yaojingyu1@huawei.com>
2025-09-11 21:58:36 +0800

    realize mix strategy for multichannel
    Signed-off-by: yaojingyu2008 <yaojingyu1@huawei.com>

--- services/audio_policy/server/infra/datashare/src/audio_policy_utils.cpp
+++ services/audio_policy/server/infra/datashare/src/audio_policy_utils.cpp
@@ -797,11 +797,12 @@ void AudioPolicyUtils::UpdateMultiChannelModuleInfo(AudioPipeInfo &info,
     std::shared_ptr<PipeStreamPropInfo> streamPropInfo)
 {
     if (!AudioPolicyManagerFactory::GetAudioPolicyManager().
-        IsChannelLayoutSupportForDspEffect(streamPropInfo->channelLayout)) {
-        AUDIO_INFO_LOG("not support channelLayout:%{public}" PRIu64 ", use default 5.1", channelLayout);
+        IsChannelLayoutSupportForDspEffect(streamPropInfo->channelLayout_)) {
+        AUDIO_INFO_LOG("not support channelLayout:%{public}" PRIu64 , streamPropInfo->channelLayout_);
         info.moduleInfo_.bufferSize =
             std::to_string(((streamPropInfo->bufferSize_ * static_cast<uint8_t>(CHANNEL_6)) /
             std::stoul(info.moduleInfo_.channels)));
+        // use default 5.1 channel for multi-channel pipe
         info.moduleInfo_.channels = std::to_string(static_cast<uint8_t>(CHANNEL_6));
         info.moduleInfo_.channelLayout = std::to_string(static_cast<uint64_t>(CH_LAYOUT_5POINT1));
     }
